35beee9b858a55cd3fe841e11eee5ecf151aafc7
David <ing.hobbit@gmail.com>
2017-01-03 10:11:49 -0600

    creacion forma RiesgoTipo

---
+++ sigpro/src/dao/RiesgoTipoDAO.java
@@ -0,0 +1,163 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import pojo.RiesgoTipo;
+import pojo.RtipoPropiedad;
+import utilities.CHibernateSession;
+import utilities.CLogger;
+
+
+public class RiesgoTipoDAO {
+	
+	public static List<RiesgoTipo> getRiesgoTipos(){
+		List<RiesgoTipo> ret = new ArrayList<RiesgoTipo>();
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+
+			CriteriaQuery<RiesgoTipo> criteria = builder.createQuery(RiesgoTipo.class);
+			Root<RiesgoTipo> root = criteria.from(RiesgoTipo.class);
+			criteria.select( root ).where(builder.equal(root.get("estado"),1));
+			ret = session.createQuery( criteria ).getResultList();
+		}
+		catch(Throwable e){
+			CLogger.write("1", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+
+	public static RiesgoTipo getRiesgoTipoPorId(int id){
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		RiesgoTipo ret = null;
+		try{
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+
+			CriteriaQuery<RiesgoTipo> criteria = builder.createQuery(RiesgoTipo.class);
+			Root<RiesgoTipo> root = criteria.from(RiesgoTipo.class);
+			criteria.select( root );
+			criteria.where( builder.and(builder.equal( root.get("id"), id ),builder.equal(root.get("estado"), 1)));
+			ret = session.createQuery( criteria ).getSingleResult();
+		}
+		catch(Throwable e){
+			CLogger.write("2", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+	
+	public static boolean guardarRiesgoTipo(RiesgoTipo riesgotipo){
+		boolean ret = false;
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			session.beginTransaction();
+			session.saveOrUpdate(riesgotipo);
+			session.flush();
+			
+			for (RtipoPropiedad propiedad : riesgotipo.getRtipoPropiedads()){
+				session.saveOrUpdate(propiedad);	
+			}
+			session.flush();
+			session.getTransaction().commit();
+			ret = true;
+		}
+		catch(Throwable e){
+			CLogger.write("3", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+	
+	
+	
+	public static boolean eliminarRiesgoTipo(RiesgoTipo riesgoTipo){
+		boolean ret = false;
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			
+			riesgoTipo.setEstado(0);
+			session.beginTransaction();
+			session.update(riesgoTipo);
+			session.getTransaction().commit();
+			ret = true;
+		}
+		catch(Throwable e){
+			CLogger.write("4",RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+	
+	
+	
+	public static boolean eliminarTotalRiesgoTipo(RiesgoTipo riesgoTipo){
+		boolean ret = false;
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			session.beginTransaction();
+			session.delete(riesgoTipo);
+			session.getTransaction().commit();
+			ret = true;
+		}
+		catch(Throwable e){
+			CLogger.write("5", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+	
+	
+	public static List<RiesgoTipo> getRiesgoTiposPagina(int pagina, int numeroriesgostipo){
+		List<RiesgoTipo> ret = new ArrayList<RiesgoTipo>();
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			Query<RiesgoTipo> criteria = session.createQuery("SELECT r FROM RiesgoTipo r WHERE estado = 1",RiesgoTipo.class);
+			criteria.setFirstResult(((pagina-1)*(numeroriesgostipo)));
+			criteria.setMaxResults(numeroriesgostipo);
+			ret = criteria.getResultList();
+		}
+		catch(Throwable e){
+			CLogger.write("6", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+	
+	public static Long getTotalRiesgoTipo(){
+		Long ret=0L;
+		Session session = CHibernateSession.getSessionFactory().openSession();
+		try{
+			Query<Long> conteo = session.createQuery("SELECT count(r.id) FROM RiesgoTipo r WHERE r.estado=1",Long.class);
+			ret = conteo.getSingleResult();
+		}
+		catch(Throwable e){
+			CLogger.write("7", RiesgoTipoDAO.class, e);
+		}
+		finally{
+			session.close();
+		}
+		return ret;
+	}
+
+}
--- sigpro/src/servlets/SRiesgoTipo.java
+++ sigpro/src/servlets/SRiesgoTipo.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Type;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.zip.GZIPOutputStream;
 
 import javax.servlet.ServletException;
@@ -14,13 +17,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.http.HttpSession;
 
-
-
+import org.joda.time.DateTime;
 
 import com.google.gson.Gson;
-
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+
+import dao.RiesgoTipoDAO;
+import dao.RtipoPropiedadDAO;
+import pojo.RiesgoPropiedad;
+import pojo.RiesgoTipo;
+import pojo.RtipoPropiedad;
+import pojo.RtipoPropiedadId;
+import utilities.Utils;
 
 /**
  * Servlet implementation class SRiesgoTipo
@@ -28,6 +39,16 @@ import com.google.gson.reflect.TypeToken;
 @WebServlet("/SRiesgoTipo")
 public class SRiesgoTipo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	class striesgotipo{
+		Integer id;
+		String nombre;
+		String descripcion;
+		String usuarioCreo;
+		String usuarioActualizo;
+		String fechaCreacion;
+		String fechaActualizacion;
+		int estado;
+	}
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -48,6 +69,8 @@ public class SRiesgoTipo extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		HttpSession sesionweb = request.getSession();
+		String usuario = sesionweb.getAttribute("usuario")!= null ? sesionweb.getAttribute("usuario").toString() : null;
 		
 		Gson gson = new Gson();
 		Type type = new TypeToken<Map<String, String>>(){}.getType();
@@ -61,20 +84,108 @@ public class SRiesgoTipo extends HttpServlet {
 		Map<String, String> map = gson.fromJson(sb.toString(), type);
 		String accion = map.get("accion");
 		String response_text="";
+		
 		if(accion.equals("getComponentetiposPagina")){
+			int pagina = map.get("pagina")!=null  ? Integer.parseInt(map.get("pagina")) : 0;
+			int numeroRiesgoTipo = map.get("numeroriesgostipo")!=null  ? Integer.parseInt(map.get("numeroriesgostipo")) : 0;
+			List<RiesgoTipo> riesgotipos = RiesgoTipoDAO.getRiesgoTiposPagina(pagina, numeroRiesgoTipo);
+			List<striesgotipo> striesgotipos=new ArrayList<striesgotipo>();
+			for(RiesgoTipo riesgotipo:riesgotipos){
+				striesgotipo temp =new striesgotipo();
+				temp.descripcion = riesgotipo.getDescripcion();
+				temp.estado = riesgotipo.getEstado();
+				temp.fechaActualizacion = Utils.formatDate(riesgotipo.getFechaActualizacion());
+				temp.fechaCreacion = Utils.formatDate(riesgotipo.getFechaCreacion());
+				temp.id = riesgotipo.getId();
+				temp.nombre = riesgotipo.getNombre();
+				temp.usuarioActualizo = riesgotipo.getUsuarioActualizo();
+				temp.usuarioCreo = riesgotipo.getUsuarioCreo();
+				striesgotipos.add(temp);
+			}
 			
+			response_text=new GsonBuilder().serializeNulls().create().toJson(striesgotipos);
+	        response_text = String.join("", "\"riesgotipos\":",response_text);
+	        response_text = String.join("", "{\"success\":true,", response_text,"}");
 		}
+		
+		else if(accion.equals("numeroRiesgoTipos")){
+			response_text = String.join("","{ \"success\": true, \"totalriesgos\":",RiesgoTipoDAO.getTotalRiesgoTipo().toString()," }");
+		}
+		else if(accion.equals("guardarRiesgotipo")){
+			boolean result = false;
+			boolean esnuevo = map.get("esnuevo").equals("true");
+			int id = map.get("id")!=null ? Integer.parseInt(map.get("id")) : 0;
+			if(id>0 || esnuevo){
+				
+				String nombre = map.get("nombre");
+				String descripcion = map.get("descripcion");
+				RiesgoTipo riesgoTipo;
+				
+				if(esnuevo){
+					riesgoTipo = new RiesgoTipo(nombre, usuario, new DateTime().toDate(), 1);
+				}
+				else{
+					riesgoTipo = RiesgoTipoDAO.getRiesgoTipoPorId(id);
+					riesgoTipo.setNombre(nombre);
+					riesgoTipo.setDescripcion(descripcion);
+					riesgoTipo.setUsuarioActualizo(usuario);
+					riesgoTipo.setFechaActualizacion(new DateTime().toDate());
+					Set<RtipoPropiedad> propiedades_temp = riesgoTipo.getRtipoPropiedads();
+					riesgoTipo.setRtipoPropiedads(null);
+					if (propiedades_temp!=null){
+						for (RtipoPropiedad rtipoPropiedad : propiedades_temp){
+							RtipoPropiedadDAO.eliminarTotalRtipoPropiedad(rtipoPropiedad);
+						}
+					}
+				}
+				
+				result = RiesgoTipoDAO.guardarRiesgoTipo(riesgoTipo);
+				
+				String[] idsPropiedades =  map.get("propiedades") != null ? map.get("propiedades").toString().split(",") : null;
+				if (idsPropiedades !=null && idsPropiedades.length>0){
+					for (String idPropiedad : idsPropiedades){
+						RtipoPropiedadId rtipoPropiedadId = new RtipoPropiedadId(riesgoTipo.getId(), Integer.parseInt(idPropiedad));
+						RiesgoPropiedad riesgoPropiedad = new RiesgoPropiedad();
+						riesgoPropiedad.setId(Integer.parseInt(idPropiedad));
+						
+						RtipoPropiedad rtipoPropiedad = new RtipoPropiedad(
+								rtipoPropiedadId, riesgoPropiedad, 
+								riesgoTipo, usuario, new DateTime().toDate(),1);
+						
+						rtipoPropiedad.setRiesgoTipo(riesgoTipo);
+						if (riesgoTipo.getRtipoPropiedads() == null){
+							riesgoTipo.setRtipoPropiedads(new HashSet<RtipoPropiedad>(0));
+						}
+						riesgoTipo.getRtipoPropiedads().add(rtipoPropiedad);
+					}
+				}
+				
+				result = RiesgoTipoDAO.guardarRiesgoTipo(riesgoTipo);
+				response_text = String.join("","{ \"success\": ",(result ? "true" : "false"),", "
+						+ "\"id\": " + riesgoTipo.getId() +" }");
+			}
+			else
+				response_text = "{ \"success\": false }";
+		}
+		else if(accion.equals("borrarRiesgoTipo")){
+			int id = map.get("id")!=null ? Integer.parseInt(map.get("id")) : 0;
+			if(id>0){
+				RiesgoTipo riesgoTipo = RiesgoTipoDAO.getRiesgoTipoPorId(id);
+				response_text = String.join("","{ \"success\": ",(RiesgoTipoDAO.eliminarRiesgoTipo(riesgoTipo) ? "true" : "false")," }");
+			}
+			else
+				response_text = "{ \"success\": false }";
+		}else
+			response_text = "{ \"success\": false }";
+		
 		
 		response.setHeader("Content-Encoding", "gzip");
 		response.setCharacterEncoding("UTF-8");
-		
-        
+		        
         OutputStream output = response.getOutputStream();
 		GZIPOutputStream gz = new GZIPOutputStream(output);
         gz.write(response_text.getBytes("UTF-8"));
         gz.close();
         output.close();
 	}
-	
-
 }
