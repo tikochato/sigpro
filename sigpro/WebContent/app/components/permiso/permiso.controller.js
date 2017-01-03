2d2c8615f3f34d256ddc92c01495934f059aa0c6
dlobregon <troyanrex@gmail.com>
2017-01-03 09:26:08 -0600

    actualizacion de tabulaciones

--- sigpro/WebContent/app/components/permiso/permiso.controller.js
+++ sigpro/WebContent/app/components/permiso/permiso.controller.js
@@ -1,95 +1,90 @@
 var app = angular.module('permisoController', [ 'ngTouch', 'ui.grid.edit' ]);
 
-app
-		.controller(
-				'permisoController',
-				[
-						'$scope',
-						'$http',
-						'$interval',
-						'$q',
-						function($scope, $http, $interval, $q) {
-							var mi=this;
-							mi.entityselected = null;
-							mi.isNew = false;
-							mi.fields = {};
+app.controller(
+ 'permisoController',
+ [
+  '$scope',
+  '$http',
+  '$interval',
+  '$q',
+  function($scope, $http, $interval, $q) {
+	var mi=this;
+	mi.entityselected = null;
+	mi.isNew = false;
+	mi.fields = {};
 
-							/*$http.post('/SPermiso',
-									{ action : 'load' }).success(function(data) {
-										mi.fields = data;
-							});*/
+	/*$http.post('/SPermiso',
+	{ action : 'load' }).success(function(data) {
+	mi.fields = data;
+	});*/
 
-							mi.today = function() {
-								mi.dt = new Date();
-							};
+	mi.today = function() {
+		mi.dt = new Date();
+	};
 							
-							mi.today();
-
+	mi.today();
 							/*mi.gridOptions = {
 								enableRowSelection : true,
 								enableRowHeaderSelection : false
 							};*/
 
-							mi.gridOptions = {
-								enableRowSelection : true,
-								enableRowHeaderSelection : false,
-								paginationPageSizes : [ 25, 50, 75 ],
-								paginationPageSize : 25,
-								data : [],
-								columnDefs : [ {
-									name : 'Id',
-									field : 'id'
-								}, {
-									name : 'Nombres',
-									field : 'nombre'
-								}, {
-									name : 'Descripcion',
-									field : 'descripcion'
-								}
+	mi.gridOptions = {
+		enableRowSelection : true,
+		enableRowHeaderSelection : false,
+		paginationPageSizes : [ 25, 50, 75 ],
+		paginationPageSize : 25,
+		data : [],
+		columnDefs : [ {
+			name : 'Id',
+			field : 'id'
+		}, {
+			name : 'Nombres',
+			field : 'nombre'
+		}, {
+			name : 'Descripcion',
+			field : 'descripcion'
+		}
 
-								],
+		],
 
-							};
-							$http.post('/SPermiso',
-									{ action : 'getPermisos' }).success(function(data) {
-										mi.gridOptions.data =  data.permisos;
-							});
+	};
+	$http.post('/SPermiso',
+			{ action : 'getPermisos' }).success(function(data) {
+			mi.gridOptions.data =  data.permisos;
+	});
+	mi.gridOptions.multiSelect = false;
+	mi.gridOptions.modifierKeysToMultiSelect = false;
+	mi.gridOptions.noUnselect = true;
+	mi.gridOptions.onRegisterApi = function(gridApi) {
+		mi.gridApi = gridApi;
+		gridApi.selection.on
+		.rowSelectionChanged(
+			$scope,
+			function(row) {
+			var msg = 'row selected '
+			+ row;
 
-							mi.gridOptions.multiSelect = false;
-							mi.gridOptions.modifierKeysToMultiSelect = false;
-							mi.gridOptions.noUnselect = true;
-							mi.gridOptions.onRegisterApi = function(gridApi) {
-								mi.gridApi = gridApi;
+			mi.permisoSelected = row.entity;
+    		console.log(row);
+			//mi.gender.selected = mi.entityselected.gender;
+		});
 
-								gridApi.selection.on
-										.rowSelectionChanged(
-												$scope,
-												function(row) {
-													var msg = 'row selected '
-															+ row;
+	};
 
-													mi.permisoSelected = row.entity;
-													console.log(row);
-													//mi.gender.selected = mi.entityselected.gender;
-												});
+    mi.open2 = function() {
+    	mi.popup2.opened = true;
+	};
 
-							};
+	mi.dateOptions = {
+		formatYear : 'yy',
+		maxDate : new Date(2020, 5, 22),
+		minDate : new Date(1900, 1, 1),
+		startingDay : 1
+	};
 
-							mi.open2 = function() {
-								mi.popup2.opened = true;
-							};
-
-							mi.dateOptions = {
-
-								formatYear : 'yy',
-								maxDate : new Date(2020, 5, 22),
-								minDate : new Date(1900, 1, 1),
-								startingDay : 1
-							};
-
-							mi.popup2 = {
-								opened : false
-							};
+	mi.popup2 = {
+ 	  opened : false
+	};
 							
 							/*
 							mi.save = function() {
@@ -162,70 +157,60 @@ app
 								}
 							}*/
 
-							mi.deleteRow = function() {
+	mi.deleteRow = function() {
 
-								var param_data = {
-									action : 'delete',
-									emp_no : mi.entityselected.id
-								};
-								$http
-										.post(
-												'/SABCGrid',
-												param_data)
-										.then(
-												function(response) {
+		var param_data = {
+			action : 'delete',
+			emp_no : mi.entityselected.id
+		};
+		$http.post(
+			'/SABCGrid',
+			param_data).then(
+			 function(response) {
+				if (response.data.success) {
+					if (mi.entityselected != null) {
+						var index = mi.gridOptions.data.indexOf(mi.entityselected);
+						if (index >= 0) {
+							mi.gridOptions.data.splice(index,1);
+						}
+						mi.entityselected == null;
 
-													if (response.data.success) {
-														if (mi.entityselected != null) {
-															var index = mi.gridOptions.data
-																	.indexOf(mi.entityselected);
-															if (index >= 0) {
-																mi.gridOptions.data
-																		.splice(
-																				index,
-																				1);
-															}
-															mi.entityselected == null;
+					}
+				}
 
-														}
+		});
 
-													}
+	};
 
-												});
+	mi.newRow = function() {
+		var formularios = loadform();						
+		mi.isCollapsed = true;
+		mi.entityselected = null;
+		mi.isNew = true;
+	}
 
-							};
+    mi.editRow = function() {
+		mi.isCollapsed = true;
+		mi.isNew = false;
+	}
 
-							mi.newRow = function() {
-
-								var formularios = loadform();
-								
-								mi.isCollapsed = true;
-								mi.entityselected = null;
-								mi.isNew = true;
-							}
-
-							mi.editRow = function() {
-								mi.isCollapsed = true;
-								mi.isNew = false;
-							}
-
-							mi.cancel = function() {
-								mi.isCollapsed = false;
-								mi.entityselected = null;
-							}
+	mi.cancel = function() {
+		mi.isCollapsed = false;
+		mi.entityselected = null;
+	}
 							
-							var deferred = $q.defer();
-							mi.currDate = new Date();
-							mi.onlyNumbers = /^[0-9]+$/;
-
-							function loadform() {
-								$http.post('/SPermiso',
-										{action: 'getPermisos'}).success(function(data) {
-									mi.fields = data;
-								});
-							};
+	var deferred = $q.defer();
+	mi.currDate = new Date();
+	mi.onlyNumbers = /^[0-9]+$/;
+	function loadform() {
+		$http.post('/SPermiso',
+			{action: 'getPermisos'}).success(
+				function(data) {
+					mi.fields = data;
+		});
+	};
 
 							
 
 
-						} ]);
+} ]);
