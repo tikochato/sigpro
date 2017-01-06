5d6bc950e2bd6673966fb942b484b789590af25f
dlobregon <troyanrex@gmail.com>
2017-01-06 12:04:36 -0600

    renombre de archivos

--- sigpro/WebContent/app/components/usuarios/usuario.controller.js
+++ sigpro/WebContent/app/components/usuarios/usuario.controller.js
@@ -26,7 +26,7 @@ app.controller(
 	mi.numeroMaximoPaginas = $utilidades.numeroMaximoPaginas;
 	mi.elementosPorPagina = $utilidades.elementosPorPagina;
 	mi.permisoSelected={id:"",nombre:"", descripcion:""};	
-	mi.usuariosSelected={usuario:""};
+	mi.usuariosSelected={usuario:"", email:"",password:""};
 	mi.gridOptions = {
 		enableRowSelection : true,
 		enableRowHeaderSelection : false,
@@ -34,23 +34,28 @@ app.controller(
 		paginationPageSize : 25,
 		data : [],
 		columnDefs : [ {
-			name : 'usuario',
+			name : 'Usuario',
 			field : 'usuario'
 		}, {
-			name : 'Nombre',
-			field : 'nombre'
+			name : 'Correo',
+			field : 'email'
 		}, {
-			name : 'Descripcion',
-			field : 'descripcion'
+			name : 'Fecha Creación',
+			field : 'fechaCreacion'
+		}, {
+			name: 'Usuario creó',
+			field: 'usuarioCreo'
+		},{
+			name: 'Usuario Actualizó',
+			field: 'usuarioActualizo'
 		}
-
 		],
 
 	};
 	mi.cargarTabla=function(pagina){
-		$http.post('/SPermiso',
-				{ accion : 'getPermisosPagina',  pagina: pagina, numeroPermisos: $utilidades.elementosPorPagina  }).success(function(data) {
-				mi.gridOptions.data =  data.permisos;
+		$http.post('/SUsuario',
+				{ accion : 'getUsuarios',  pagina: pagina, numeroUsuarios: $utilidades.elementosPorPagina  }).success(function(data) {
+				mi.gridOptions.data =  data.usuarios;
 				mi.mostrarcargando=false;
 		});
 	};
@@ -72,7 +77,7 @@ app.controller(
 			mi.guardarEstado();
 	    }
 	    else{
-	    	  $http.post('/SEstadoTabla', { action: 'getEstado', grid:'permisos', t: (new Date()).getTime()}).then(function(response){
+	    	  $http.post('/SEstadoTabla', { action: 'getEstado', grid:'usuarios', t: (new Date()).getTime()}).then(function(response){
 		      if(response.data.success && response.data.estado!='')
 		    	  mi.gridApi.saveState.restore( $scope, response.data.estado);
 		    	  mi.gridApi.colMovable.on.columnPositionChanged($scope, mi.guardarEstado);
@@ -94,14 +99,14 @@ app.controller(
 		mi.isCollapsed = false;
 	}
 	
-	mi.nuevoPermiso=function(){
+	/*mi.nuevoPermiso=function(){
 		var formularios = mi.cargarTabla(mi.paginaActual);						
 		mi.isCollapsed = true;
 		mi.entityselected = null;
 		mi.esNuevo = true;
 		mi.permisoSelected = {id:"",nombre:"", descripcion:""};
-	};					
-	mi.guardarPermiso=function(){		
+	};	*/				
+	/*mi.guardarPermiso=function(){		
 		if(mi.permisoSelected.nombre!=="" && mi.permisoSelected.descripcion!==""){
 			if(mi.esNuevo){
 				$http.post('/SPermiso',
@@ -139,8 +144,9 @@ app.controller(
 		}else{
 			$utilidades.mensaje('danger','Llene los campos');
 		}
-	};
-	mi.borrarPermiso=function(ev){
+	};*/
+	
+	/*mi.borrarPermiso=function(ev){
 		mi.isCollapsed = false;
 		if(mi.permisoSelected.id!==""){
 			var confirm = $mdDialog.confirm()
@@ -169,24 +175,24 @@ app.controller(
 		}else{
 		    $utilidades.mensaje('danger','Seleccione un permiso');
 		}
-	};
+	};*/
 	
 	
-	mi.editarPermiso=function(){
+	/*mi.editarPermiso=function(){
 		if(mi.permisoSelected.id!==""){
 			mi.isCollapsed = true;
 			mi.esNuevo=false;
 		}else{
 			$utilidades.mensaje('danger','Seleccione un permiso');
 		}
-	};
+	};*/
 	
-	mi.reiniciarVista=function(){
+	/*mi.reiniciarVista=function(){
 		if($location.path()=='/permisos/rv')
 			$route.reload();
 		else
 			$location.path('/permisos/rv');
-	};
+	};*/
 	
 	mi.cambiarPagina=function(){
 		mi.cargarTabla(mi.paginaActual);
@@ -194,15 +200,15 @@ app.controller(
 	
 	mi.guardarEstado=function(){
 		var estado = mi.gridApi.saveState.save();
-		var tabla_data = { action: 'guardaEstado', grid:'permisos', estado: JSON.stringify(estado), t: (new Date()).getTime() }; 
+		var tabla_data = { action: 'guardaEstado', grid:'usuarios', estado: JSON.stringify(estado), t: (new Date()).getTime() }; 
 		$http.post('/SEstadoTabla', tabla_data).then(function(response){
 			
 		});
 	}
 	
-	$http.post('/SPermiso', { accion: 'getTotalPermisos' }).success(
+	$http.post('/SUsuario', { accion: 'getTotalUsuarios' }).success(
 			function(response) {
-				mi.totalPermisos = response.totalPermisos;
+				mi.totalUsuarios = response.totalUsuarios;
 				mi.cargarTabla(mi.paginaActual);
 	});
 } ]);
