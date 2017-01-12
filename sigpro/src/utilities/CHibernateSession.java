40c6cb98cd2f25163a61fb990c0d3f9a47172ab8
dlobregon <troyanrex@gmail.com>
2017-01-12 12:21:35 -0600

    se actualizo el manejo de session de acuerdo si está en pruebas unitarias o en produccion/desarrollo

--- sigpro/src/utilities/CHibernateSession.java
+++ sigpro/src/utilities/CHibernateSession.java
@@ -10,33 +10,40 @@ import org.hibernate.cfg.Configuration;
 
 public class CHibernateSession {
 
-	private static final SessionFactory sessionFactory;
-
+	private static  SessionFactory sessionFactory;
+	private static boolean testState = TestSetUp.getTestState();
+	
 	static {
-		try {
-			Configuration configuration = new Configuration();
-
+		if(!testState){
 			try {
-				InitialContext context = new InitialContext();
-			    DataSource ds = (DataSource) context.lookup("java:comp/env/jdbc/sigpro");
-		    	Connection conn = ds.getConnection();
-		    	conn.isClosed();
-		    	configuration.configure();
-		    	conn.close();
-		    }
-		    catch(Exception e){
-		    	configuration.configure("hibernate_local.cfg.xml");
-		    }
-		    
-		    sessionFactory = configuration.buildSessionFactory();
-		    
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
+				Configuration configuration = new Configuration();
+
+				try {
+					InitialContext context = new InitialContext();
+				    DataSource ds = (DataSource) context.lookup("java:comp/env/jdbc/sigpro");
+			    	Connection conn = ds.getConnection();
+			    	conn.isClosed();
+			    	configuration.configure();
+			    	conn.close();
+			    }
+			    catch(Exception e){
+			    	configuration.configure("hibernate_local.cfg.xml");
+			    }
+			    
+			    sessionFactory = configuration.buildSessionFactory();
+			    
+			} catch (Throwable ex) {
+				System.err.println("Initial SessionFactory creation failed." + ex);
+				throw new ExceptionInInitializerError(ex);
+			}
 		}
 	}
 
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
+	}
+	
+	public static void changeEnviroment(SessionFactory session){
+		sessionFactory = session;
 	}
 }
