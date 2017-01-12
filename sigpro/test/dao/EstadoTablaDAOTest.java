8fb459dc68f827918878ad78e4289962526045c1
dlobregon <troyanrex@gmail.com>
2017-01-12 12:34:24 -0600

    defincion de suite de pruebas

--- sigpro/test/dao/EstadoTablaDAOTest.java
+++ sigpro/test/dao/EstadoTablaDAOTest.java
@@ -2,32 +2,11 @@ package dao;
 
 import static org.junit.Assert.*;
 
-
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.junit.Before;
 import org.junit.Test;
-import java.io.File;
 import pojo.EstadoTabla;
 import pojo.EstadoTablaId;
-import utilities.CHibernateSession;
-import utilities.TestSetUp;
 
 public class EstadoTablaDAOTest {
-	private static Configuration config;
-    private static SessionFactory factory;	
-   
-
-	@Before
-	public void setUp() throws Exception {
-		config = new Configuration();
-        config.configure(new File("src/hibernate_tmp.cfg.xml"));
-        factory = config.buildSessionFactory();
-        TestSetUp.setTestState();
-        CHibernateSession.changeEnviroment(factory);
-   	}
 
 	@Test
 	public void testSaveEstadoTabla() {
---
+++ sigpro/test/dao/TestDAOSuite.java
@@ -0,0 +1,32 @@
+package dao;
+import java.io.File;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import utilities.CHibernateSession;
+import utilities.TestSetUp;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EstadoTablaDAOTest.class })
+public class TestDAOSuite {
+	private static Configuration config;
+    private static SessionFactory factory;	
+   
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		config = new Configuration();
+        config.configure(new File("src/hibernate_tmp.cfg.xml"));
+        factory = config.buildSessionFactory();
+        TestSetUp.setTestState();
+        CHibernateSession.changeEnviroment(factory);
+   	}
+
+	
+}
