51aa5014bc65254d8ffb15e571be83024b7d8950
dlobregon <troyanrex@gmail.com>
2017-01-13 15:39:14 -0600

    componenteDAO prueba unitaria

--- sigpro/test/dao/TestDAOSuite.java
+++ sigpro/test/dao/TestDAOSuite.java
@@ -24,6 +24,7 @@ import utilities.TestSetUp;
 	,ProyectoTipoDAOTest.class
 	,RiesgoTipoDAOTest.class
 	,ColaboradorDAOTest.class
+	,ComponenteDAOTest.class
 	})
 public class TestDAOSuite {
 	private static Configuration config;
