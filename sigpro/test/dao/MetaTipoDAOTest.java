7bf0870bbd4d78844800cb1f060c4c1b13258108
dlobregon <troyanrex@gmail.com>
2017-01-16 12:09:23 -0600

    se cambio validacion de metatipo

--- sigpro/test/dao/MetaTipoDAOTest.java
+++ sigpro/test/dao/MetaTipoDAOTest.java
@@ -2,21 +2,8 @@ package dao;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import pojo.MetaTipo;
-import utilities.CHibernateSession;
-import utilities.TestSetUp;
 
 public class MetaTipoDAOTest {
 
@@ -31,12 +18,7 @@ public class MetaTipoDAOTest {
 	}
 	@Test
 	public void guardarMetaTipoTest(){
-		MetaTipo metaTipo = new MetaTipo();
-		metaTipo.setNombre("unit_test");
-		metaTipo.setDescripcion("unit_test");
-		metaTipo.setUsuarioCreo("admin");
-		metaTipo.setEstado(1);
-		assertNotNull(MetaTipoDAO.guardarMetaTipo(metaTipo));
+		assertEquals(MetaTipoDAO.guardarMetaTipo(new MetaTipo()),false);
 	}
 	@Test
 	public void eliminarMetaTipoTest(){
@@ -58,23 +40,5 @@ public class MetaTipoDAOTest {
 		assertEquals(MetaTipoDAO.getTotalMetaTipos().getClass(),Long.class);
 	}
 	
-	@AfterClass
-	public static void cleanData(){
-		Session session = CHibernateSession.getSessionFactory().openSession();
-		try{
-			CriteriaBuilder builder = session.getCriteriaBuilder();
-			CriteriaQuery<MetaTipo> criteria = builder.createQuery(MetaTipo.class);
-			Root<MetaTipo> root = criteria.from(MetaTipo.class);
-			criteria.where( builder.and(builder.equal(root.get("nombre"), "unit_test")));
-			MetaTipo MetaTipoTmp =(MetaTipo) session.createQuery( criteria ).getSingleResult();
-			session.beginTransaction();
-			session.delete(MetaTipoTmp);
-			session.getTransaction().commit();
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally{
-			session.close();
-		}
-		
-	}
+	
 }
