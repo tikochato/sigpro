60fdf09e7b6b21e25ab4d96eb867e07b058862b8
dlobregon <troyanrex@gmail.com>
2017-01-17 12:39:27 -0600

    pruebas unitarias de servlet usuario completas

--- sigpro/test/servlets/SUsuarioTest.java
+++ sigpro/test/servlets/SUsuarioTest.java
@@ -7,17 +7,48 @@ import utils.ClienteHttp;
 import utils.DecodificadorJson;
 public class SUsuarioTest {
 
-	
+	String direccionServlet = "http://localhost:8080/SUsuario";
 	@Test
 	public void getUsuarioTest(){
-		String resultado  = ClienteHttp.peticionHttp("http://localhost:8080/SUsuario", "{\"accion\":\"getUsuarios\", \"pagina\":\"1\" ,  \"numeroUsuarios\":\"1\"}");
-		System.out.println(resultado);
+		String resultado  = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"getUsuarios\", \"pagina\":\"1\" ,  \"numeroUsuarios\":\"1\"}");
 		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "true");
 
 	}
 	
-	/*@Test
+	@Test
 	public void getTotalUsuarioTest(){
-		String resultado;
-	}*/
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"registroUsuario\", \"usuario\":\"\" ,  \"password\":\"\" ,  \"email\":\"\"  ,  \"permisos\":\"[]\"    }");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "false");
+	}
+	
+	@Test
+	public void actualizarPermisosTest(){
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"actualizarPermisos\", \"usuario\":\"unit_test\" ,  \"permisosNuevos\":\"[]\" ,    \"permisosEliminados\":\"[]\"    }");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "false");
+	}
+	
+	@Test 
+	public void eliminarUsuarioTest(){
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"eliminarUsuario\", \"usuario\":\"unit_test\"}");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "false");
+	}
+	
+	@Test
+	public void obtenerPermisosTest(){
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"obtenerPermisos\", \"usuario\":\"unit_test\"}");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "true");
+	}
+	@Test
+	public void editarUsuarioTest(){
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"editarUsuario\", \"usuario\":\"unit_test\" , \"email\":\"unit_test.\" }");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "false");
+	}
+	
+	@Test
+	public void cambiarPasswordTest(){
+		String resultado = ClienteHttp.peticionHttp(direccionServlet, "{\"accion\":\"cambiarPassword\", \"usuario\":\"unit_test\" , \"password\":\"unit_test.\" }");
+		assertEquals(DecodificadorJson.decodificarObjeto(resultado, "success"), "false");
+	}
+	
+	
 }
