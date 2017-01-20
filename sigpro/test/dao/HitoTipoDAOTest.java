95967f5224475aba3a1de0b1a9398058e707de62
dlobregon <troyanrex@gmail.com>
2017-01-20 01:54:01 -0600

    funciones nuevas

--- sigpro/test/dao/HitoTipoDAOTest.java
+++ sigpro/test/dao/HitoTipoDAOTest.java
@@ -1,10 +1,7 @@
 package dao;
 
 import static org.junit.Assert.*;
-import java.util.HashSet;
 import org.junit.Test;
-
-import pojo.Hito;
 import pojo.HitoTipo;
 
 public class HitoTipoDAOTest {
@@ -21,18 +18,17 @@ public class HitoTipoDAOTest {
 	
 	@Test
 	public void guardarHitoTipoTest(){
-		HitoTipo hitoTipo = new HitoTipo("unit_test","unit_test",1, new HashSet<Hito>(0));
-		assertNotNull(HitoTipoDAO.guardarHitoTipo(hitoTipo));
+		assertEquals(HitoTipoDAO.guardarHitoTipo(new HitoTipo()),false);
 	}
 	
 	@Test
 	public void  eliminarHitoTipoTest(){
-		assertNotNull(HitoTipoDAO.eliminarHitoTipo( new HitoTipo("unit_test","unit_test",1, new HashSet<Hito>(0))));
+		assertEquals(HitoTipoDAO.eliminarHitoTipo(new HitoTipo()),false);
 	}
 	
 	@Test
 	public void eliminarTotalHitoTipoTest(){
-		assertNotNull(HitoTipoDAO.eliminarTotalHitoTipo( new HitoTipo("unit_test","unit_test",1, new HashSet<Hito>(0))));
+		assertNotNull(HitoTipoDAO.eliminarTotalHitoTipo( new HitoTipo()));
 	}
 	
 	@Test
