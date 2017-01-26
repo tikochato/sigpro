9d800e68e166658ea342038c6f28977c47fa2472
dlobregon <troyanrex@gmail.com>
2017-01-26 11:43:56 -0600

    eliminacion de clase auxiliar

--- sigpro/test/utils/ClienteHttp.java
+++ sigpro/test/utils/ClienteHttp.java
@@ -1,33 +1,50 @@
 package utils;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 import utilities.CLogger;
 
 public class ClienteHttp {
 	
 	public static String peticionHttp(String url, String parametros){
-		HttpClient httpClient = HttpClientBuilder.create().build(); 
-		String respuesta ="";
-		try {
-		    HttpPost request = new HttpPost(url);
-		    StringEntity params =new StringEntity(parametros);
-		    request.addHeader("content-type", "application/x-www-form-urlencoded");
-		    request.setEntity(params);
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            respuesta =httpClient.execute(request,responseHandler);
-            System.out.println(respuesta);
-
-		}catch (Throwable ex) {
-			CLogger.write("1", ClienteHttp.class, ex);
-		}
-		
-		return respuesta;
+		String ret = "";
+		try{
+			URL Url = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
+			conn.setDoOutput(true);
+			conn.setDoInput (true);
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Accept-Encoding", "gzip");
+	        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+	        conn.connect();  
+	        DataOutputStream printout =  new DataOutputStream(conn.getOutputStream ());
+	        printout.writeBytes(parametros);
+	        String salida;
+	        printout.flush ();
+	        printout.close ();
+	        BufferedReader entrada = null;
+	        if ("gzip".equals(conn.getContentEncoding())) {
+	        	entrada =new BufferedReader( new InputStreamReader(new GZIPInputStream(conn.getInputStream())));
+	        }else{
+	        	entrada = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        }
+	        StringBuffer response = new StringBuffer();  
+	        
+	        while ((salida = entrada.readLine()) != null) {  
+	         response.append(salida);  
+	        }  
+	        entrada.close();  
+	        ret = response.toString();
+		}catch(Exception e){
+			CLogger.write("1",ClienteHttp2.class,e);
+		}		
+		return ret;
+	
 	}
 	
 }
