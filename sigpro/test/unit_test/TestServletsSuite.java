61d24ef18c7984e3f105b4d52bfb87b4ab4729f1
dlobregon <troyanrex@gmail.com>
2017-01-26 11:51:44 -0600

    actualizacion de suite para pruebas unitarias de los servlets

--- sigpro/test/unit_test/TestServletsSuite.java
+++ sigpro/test/unit_test/TestServletsSuite.java
@@ -9,11 +9,37 @@ import servlets.*;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	SProyectoTipoTest.class,
-	SComponenteTipoTest.class,
-	SRiesgoTipoTest.class,
-	SHitoTipoTest.class,
-	SDesembolsoTipoTest.class
+	SColaboradorTest.class,         
+	SComponentePropiedadTest.class, 
+	SComponenteTest.class,          
+	SComponenteTipoTest.class,      
+	SCooperanteTest.class,          
+	SDatoTipoTest.class,            
+	SDesembolsoTest.class,          
+	SDesembolsoTipoTest.class,      
+	SEntidadTest.class,             
+	SEstadoTablaTest.class,         
+	SHitoTest.class,                
+	SHitoTipoTest.class,            
+	SLoginTest.class,               
+	SMetaTest.class,                
+	SMetaTipoTest.class,            
+	SMetaUnidadMedidaTest.class,    
+	SPermisoTest.class,             
+	SProductoPropiedadTest.class,   
+	SProductoTest.class,            
+	SProductoTipoTest.class,        
+	SProyectoPropiedadTest.class,   
+	SProyectoTest.class,            
+	SProyectoTipoTest.class,        
+	SRecursoPropiedadTest.class,    
+	SRecursoTipoTest.class,         
+	SRecursoUnidadMedidaTest.class, 
+	SRiesgoPropiedadTest.class,     
+	SRiesgoTest.class,              
+	SRiesgoTipoTest.class,          
+	SUnidadEjecutoraTest.class,     
+	SUsuarioTest.class  
 	})
 public class TestServletsSuite {
 
