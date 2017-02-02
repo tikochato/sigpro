80f545bbf972ef334149a8fa62ce805406dcee2c
dlobregon <troyanrex@gmail.com>
2017-02-02 13:07:02 -0600

    eliminacion de comentarios y dependencias no utilizadas

--- sigpro/src/dao/ProductoDAO.java
+++ sigpro/src/dao/ProductoDAO.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
