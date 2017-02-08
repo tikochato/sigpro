f35e64acc5ff5e59b0ed887657dce261b4ababc5
dlobregon <troyanrex@gmail.com>
2017-02-08 15:44:54 -0600

    reinicio de variables

--- sigpro/WebContent/app/components/recursotipo/recursotipo.controller.js
+++ sigpro/WebContent/app/components/recursotipo/recursotipo.controller.js
@@ -193,6 +193,7 @@ app.controller('recursotipoController',['$scope','$http','$interval','i18nServic
 		mi.irATabla = function() {
 			mi.mostraringreso=false;
 			mi.esnuevo =false;
+			mi.recursotipo={};
 		}
 		
 		mi.guardarEstado=function(){
