ecc116f34834107a4127d1d70a499dc110ff658a
dlobregon <troyanrex@gmail.com>
2017-05-01 22:06:23 -0600

    componentepropiedad ui

--- sigpro/WebContent/app/components/componentepropiedad/componentepropiedad.controller.js
+++ sigpro/WebContent/app/components/componentepropiedad/componentepropiedad.controller.js
@@ -1,4 +1,4 @@
-var app = angular.module('componentepropiedadController', []);
+var app = angular.module('componentepropiedadController', ['ui.bootstrap.contextMenu']);
 
 app.controller('componentepropiedadController',['$scope','$http','$interval','i18nService','Utilidades','$routeParams','$window','$location','$route','uiGridConstants','$mdDialog',
 		function($scope, $http, $interval,i18nService,$utilidades,$routeParams,$window,$location,$route,uiGridConstants,$mdDialog) {
@@ -22,6 +22,20 @@ app.controller('componentepropiedadController',['$scope','$http','$interval','i1
 			mi.ordenDireccion = null;
 			mi.filtros = [];
 			mi.orden = null;
+			mi.menuOptions = [
+		        ['<span class="glyphicon glyphicon-pencil"> Editar', function ($itemScope, $event, modelValue, text, $li) {
+		      	  mi.editar();
+		        }],
+		        null,
+		        ['<span class="glyphicon glyphicon-trash text-danger"><font style="color: black;"> Borrar</font>', function ($itemScope, $li) {
+		      	  mi.borrar();
+		        }]
+		    ];
+			
+			mi.contextMenu = function (event) {
+		        var filaId = angular.element(event.toElement).scope().rowRenderIndex;
+		        mi.gridApi.selection.selectRow(mi.gridOptions.data[filaId]);
+		    };
 			
 			mi.gridOptions = {
 					enableRowSelection : true,
@@ -33,6 +47,7 @@ app.controller('componentepropiedadController',['$scope','$http','$interval','i1
 					enablePaginationControls: false,
 				    paginationPageSize: $utilidades.elementosPorPagina,
 				    useExternalFiltering: true,
+				    rowTemplate: '<div context-menu="grid.appScope.componentepropiedadc.menuOptions" right-click="grid.appScope.componentepropiedadc.contextMenu($event)" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" ui-grid-one-bind-id-grid="rowRenderIndex + \'-\' + col.uid + \'-cell\'" class="ui-grid-cell ng-scope ui-grid-disable-selection grid-align-right" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }" role="gridcell" ui-grid-cell="" ></div>',
 				    useExternalSorting: true,
 					columnDefs : [ 
 						{ name: 'id', width: 100, displayName: 'ID', cellClass: 'grid-align-right', type: 'number', enableFiltering: false },
