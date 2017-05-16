877640fdd71cc89eb607939b8e010b6cdd8b80b4
david <ing.david.o@gmail.com>
2017-05-16 16:45:44 -0600

    quitando warings en codigo

--- sigpro/src/servlets/SUnidadEjecutora.java
+++ sigpro/src/servlets/SUnidadEjecutora.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.EntidadDAO;
 import dao.UnidadEjecutoraDAO;
-import pojo.UnidadEjecutora;
 import utilities.Utils;
 
 @WebServlet("/SUnidadEjecutora")
