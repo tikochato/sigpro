376c73c483261777b6aea7ff85db01d220481148
dlobregon <troyanrex@gmail.com>
2017-07-06 17:25:38 -0600

    actualizacion de permisos

--- sigpro/WebContent/app/components/usuarios/gestionUsuario.js
+++ sigpro/WebContent/app/components/usuarios/gestionUsuario.js
@@ -41,12 +41,17 @@ app.controller('gestionUsuariosController', [
 		mi.colaboradoresCargados=false;
 		mi.changeActive=function(input){
 			mi.activo=input;
-			if(input==2)
+			if(input==2){
 				mi.colaboradoresCargados=true;
+				if(!mi.seleccionPrimera){
+					mi.cargarTablaColaboradores(mi.paginaActual_);
+					mi.seleccionPrimera=true;
+				}
+			}		
 		}
 		
 		i18nService.setCurrentLang('es');
-		
+		mi.seleccionPrimera=false;
 		/**configuración de usuarios **/
 		
 		mi.mostrarcargando=true;
@@ -71,10 +76,11 @@ app.controller('gestionUsuariosController', [
 		mi.tieneColaborador=false;
 		mi.edicionPermisos=false;
 		mi.filtros=[];
+		mi.editandoUsuario=false;
 		/*mi.editarElemento = function (event) {
 	       
 	    };*/
-	    function editarElementoUsuario(event){
+	    mi.editarElementoUsuario= function(event){
 	    	 var filaId = angular.element(event.toElement).scope().rowRenderIndex;
 		        mi.gridApi.selection.selectRow(mi.gridOptions.data[filaId]);
 		        mi.editarUsuario();
@@ -94,22 +100,22 @@ app.controller('gestionUsuariosController', [
 				name : 'Usuario',
 				cellClass : 'grid-align-left',
 				field : 'usuario',
-				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'usuario\']" ng-keypress="grid.appScope.usuarioc.filtrar($event)" style="width:175px;"></input></div>'
+				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'usuario\']" ng-keypress="grid.appScope.usuarioc.filtrar(1,$event)" style="width:175px;"></input></div>'
 			}, {
 				name : 'Correo',
 				cellClass : 'grid-align-left',
 				field : 'email',
-				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'email\']" ng-keypress="grid.appScope.usuarioc.filtrar($event)" style="width:175px;"></input></div>'
+				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'email\']" ng-keypress="grid.appScope.usuarioc.filtrar(1,$event)" style="width:175px;"></input></div>'
 			}, {
 				name: 'Usuario creo',
 				cellClass : 'grid-align-left',
 				field: 'usuarioCreo',
-				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'usuario_creo\']" ng-keypress="grid.appScope.usuarioc.filtrar($event)" style="width:175px;"></input></div>'
+				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'usuario_creo\']" ng-keypress="grid.appScope.usuarioc.filtrar(1,$event)" style="width:175px;"></input></div>'
 			}, {
 				name : 'Fecha creación',
 				cellClass : 'grid-align-left',
 				field : 'fechaCreacion',
-				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'fecha_creacion\']" ng-keypress="grid.appScope.usuarioc.filtrar($event)" style="width:175px;"></input></div>'
+				filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros[\'fecha_creacion\']" ng-keypress="grid.appScope.usuarioc.filtrar(1,$event)" style="width:175px;"></input></div>'
 			},{
 				name: 'Usuario actualizo',
 				cellClass : 'grid-align-left',
@@ -168,16 +174,7 @@ app.controller('gestionUsuariosController', [
 			$window.location.href = '/main.jsp#!/forbidden';		
 		}
 
-		mi.cancelar = function() {
-			mi.isCollapsed = false;
-			mi.cambioPassword= false;
-			mi.mostrarCambioPassword = false;
-			mi.tieneColaborador=false;
-			mi.edicionPermisos=false;
-			mi.cargandoPermisos=false; 
-			mi.usuariosSelected={usuario:"", email:"",password:"", usuarioCreo:"", fechaCreacion:"", usuarioActualizo:"", fechaActualizacion:"", colaborador:""};
-		}
-
+		
 
 		mi.nuevoUsuario=function(){
 			mi.claves.password1="";
@@ -275,12 +272,15 @@ app.controller('gestionUsuariosController', [
 													if(mi.usuariosSelected.password!==passwordLocal){
 														$http.post('/SUsuario', {accion: 'cambiarPassword' , usuario: mi.usuariosSelected.usuario,	password:mi.usuariosSelected.password}).success(
 																function(response) {
+																	console.log("checkpoint");
 																	if(response.success){
 																		 $utilidades.mensaje('success', 'actualizacion de datos exitosa.');
 																	}else{
 																		$utilidades.mensaje('danger', 'No se pudo cambiar la contraseña.');
 																	}
 														});
+													}else{
+														 $utilidades.mensaje('success', 'actualizacion de datos exitosa.');
 													}
 
 												}else{
@@ -367,16 +367,12 @@ app.controller('gestionUsuariosController', [
 			}
 		};
 		
-		function validarEmail(email) {
-		    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		    return re.test(email);
-		}
 		mi.mostrarPermisos=function(){
 			mi.edicionPermisos=true;
 		};
 		mi.editarUsuario=function(){
 			if(mi.usuariosSelected.usuario!==""){
-				mi.isCollapsed = true;
+				mi.editandoUsuario = true;
 				mi.esNuevo=false;
 				passwordLocal=mi.usuariosSelected.password;
 				if(mi.usuariosSelected.colaborador!=null){
@@ -530,27 +526,171 @@ app.controller('gestionUsuariosController', [
 
 		}
 		
-		mi.filtrar = function(evt){
-			if(evt.keyCode==13){
-				$http.post('/SUsuario', { accion: 'getTotalUsuarios',	filtro_usuario: mi.filtros['usuario'],filtro_email: mi.filtros['email'],
-					filtro_usuario_creo: mi.filtros['usuario_creo'], filtro_fecha_creacion: mi.filtros['fecha_creacion']  }).success(
-						function(response) {
-							mi.elementosPorPagina = response.totalUsuarios;
-							mi.totalUsuarios =response.totalUsuarios;	
-							mi.cargarTabla(mi.paginaActual);
-							mi.gridApi.selection.clearSelectedRows();
-							mi.usuariosSelected.usuario = "";
-				});
+		mi.filtrar = function(tab,evt){
+			if(tab==1){
+				if(evt.keyCode==13){
+					$http.post('/SUsuario', { accion: 'getTotalUsuarios',	filtro_usuario: mi.filtros['usuario'],filtro_email: mi.filtros['email'],
+						filtro_usuario_creo: mi.filtros['usuario_creo'], filtro_fecha_creacion: mi.filtros['fecha_creacion']  }).success(
+							function(response) {
+								mi.elementosPorPagina = response.totalUsuarios;
+								mi.totalUsuarios =response.totalUsuarios;	
+								mi.cargarTabla(mi.paginaActual);
+								mi.gridApi.selection.clearSelectedRows();
+								mi.usuariosSelected.usuario = "";
+					});
+				}
+			}else{
+				if(evt.keyCode==13){
+					mi.cargarTablaColaboradores(mi.paginaActual_);
+				}
 			}
+			
 		};
-
-		
-		
-		
-		
-		
-		
 		/* fin de configuracion de usuarios */
+		
+		/** inicio de configuracion de colaboradores**/
+		mi.totalElementos_ = 0;
+		mi.paginaActual_ = 1;
+		mi.numeroMaximoPaginas = $utilidades.numeroMaximoPaginas;
+		mi.elementosPorPagina = $utilidades.elementosPorPagina;
+		mi.columnaOrdenada_=null;
+		mi.ordenDireccion_=null;
+		mi.filtros_ = [];
+		mi.mostrarCargando_ = true;
+		mi.data_ = [];
+		
+		mi.opcionesGrid = {
+				data : mi.data,
+				columnDefs : [{
+					displayName : 'Primer Nombre',
+					name : 'primerNombre',
+					cellClass : 'grid-align-left',
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'pnombre\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				}, {
+					displayName : 'Segundo Nombre',
+					name : 'segundoNombre',
+					cellClass : 'grid-align-left',
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'snombre\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				}, {
+					displayName : 'Primer Apellido',
+					name : 'primerApellido',
+					cellClass : 'grid-align-left',
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'papellido\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				}, {
+					displayName : 'Segundo Apellido',
+					name : 'segundoApellido',
+					cellClass : 'grid-align-left',
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'sapellido\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				}, {
+					displayName : 'CUI',
+					name : 'cui',
+					cellClass : 'grid-align-right',
+					type : 'number',
+					width : 150,
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'cui\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				},{
+					displayName : 'Nombre Unidad Ejecutora',
+					name : 'nombreUnidadEjecutora',
+					cellClass : 'grid-align-left',
+					filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.usuarioc.filtros_[\'unidad_ejecutora\']" ng-keypress="grid.appScope.usuarioc.filtrar(2,$event)"></input></div>'
+				}],
+				enableRowSelection : true,
+				enableRowHeaderSelection : false,
+				multiSelect : false,
+				modifierKeysToMultiSelect : false,
+				noUnselect : false,
+				enableFiltering : true,
+				enablePaginationControls : false,
+				paginationPageSize : $utilidades.elementosPorPagina,
+				useExternalFiltering: true,
+				useExternalSorting: true,
+				rowTemplate: '<div ng-dblclick="grid.appScope.usuarioc.editarElemento($event)" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" ui-grid-one-bind-id-grid="rowRenderIndex + \'-\' + col.uid + \'-cell\'" class="ui-grid-cell ng-scope ui-grid-disable-selection grid-align-right" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }" role="gridcell" ui-grid-cell="" ></div>',
+				onRegisterApi : function(gridApi) {
+					mi.gridApi = gridApi;
+						mi.gridApi.selection.on.rowSelectionChanged($scope,function(row){
+							mi.colaborador = row.entity;
+						});
+						
+						gridApi.core.on.sortChanged( $scope, function ( grid, sortColumns ) {
+							if(sortColumns.length==1){
+								grid.appScope.usuarioc.columnaOrdenada_=sortColumns[0].field;
+								grid.appScope.usuarioc.ordenDireccion_ = sortColumns[0].sort.direction;
+								for(var i = 0; i<sortColumns.length-1; i++)
+									sortColumns[i].unsort();
+								grid.appScope.usuarioc.cargarData(grid.appScope.usuarioc.paginaActual_);
+							}
+							else if(sortColumns.length>1){
+								sortColumns[0].unsort();
+							}
+							else{
+								if(grid.appScope.usuarioc.columnaOrdenada_!=null){
+									grid.appScope.usuarioc.columnaOrdenada_=null;
+									grid.appScope.usuarioc.ordenDireccion_=null;
+								}
+							}
+								
+						} );
+
+						/*if ($routeParams.reiniciar_vista == 'rv') {
+							mi.guardarEstado();
+							mi.obtenerTotalColaboradores();
+						} else {
+							$http.post('/SEstadoTabla', {
+								action : 'getEstado',
+								grid : 'colaborador',
+								t : (new Date()).getTime()
+							}).then(
+									function(response) {
+
+										if (response.data.success
+												&& response.data.estado != '') {
+											mi.gridApi.saveState.restore($scope,
+													response.data.estado);
+										}{
+
+											mi.gridApi.colMovable.on.columnPositionChanged(
+													$scope, mi.guardarEstado);
+											mi.gridApi.colResizable.on.columnSizeChanged(
+													$scope, mi.guardarEstado);
+											mi.gridApi.core.on.columnVisibilityChanged($scope,
+													mi.guardarEstado);
+										}
+										//mi.obtenerTotalColaboradores();
+									});
+						}*/
+					}
+		};
+		
+		mi.cargarTablaColaboradores = function(pagina) {
+			var datos = {
+				accion : 'cargar',
+				pagina : pagina,
+				filtro_pnombre: mi.filtros_['pnombre'],
+				filtro_snombre: mi.filtros_['snombre'],
+				filtro_papellido: mi.filtros_['papellido'],
+				filtro_sapellido: mi.filtros_['sapellido'],
+				filtro_cui: mi.filtros_['cui'],
+				filtro_unidad_ejecutora: mi.filtros_['unidad_ejecutora'],
+				columna_ordenada: mi.columnaOrdenada_, orden_direccion: mi.ordenDireccion_,
+				registros : mi.elementosPorPagina_
+			};
+
+			mi.mostrarCargando = true;
+			$http.post('/SColaborador', datos).then(function(response) {
+				if (response.data.success) {
+
+					mi.data_ = response.data.colaboradores;
+					mi.opcionesGrid.data = mi.data_;
+					mi.totalElementos_=response.data.colaboradores.length;
+					mi.mostrarCargando = false;
+				}
+			});
+
+		};
+		/** final de configuaración de colaboradores**/
+		
+		
+		
 		function validarEmail(email) {
 		    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		    return re.test(email);
@@ -563,6 +703,7 @@ app.controller('gestionUsuariosController', [
 				cargarTablaUsuarios(mi.paginaActual);
 			}else{
 				//carga la tabla de colaboradores
+				cargarTablaColaboradores(mi.paginaActual_);
 			}
 		};
 		
@@ -570,10 +711,10 @@ app.controller('gestionUsuariosController', [
 			if(tab==1){
 				//edita un usuario
 				if(event){
-					//qq editarElementoUsuario(event);
+					mi.editarElementoUsuario(event);
 				}else{
 					//cuando se presiona el boton de editar
-					//qq mi.editarUsuario();
+					 mi.editarUsuario();
 				}
 				
 			}else{
@@ -607,6 +748,7 @@ app.controller('gestionUsuariosController', [
 				cargarTablaUsuarios(mi.paginaActual);
 			}else{
 				//reiniciar tabla de colaborador
+				cargarTablaColaboradores(mi.paginaActual_);
 			}
 		}
 		
@@ -626,11 +768,190 @@ app.controller('gestionUsuariosController', [
 			}
 		}
 		
+		
+		mi.cancelar = function() {
+			//para cancelar a usuario
+			if(mi.editandoUsuario){
+				mi.editandoUsuario=false;
+				mi.cambioPassword= false;
+				mi.mostrarCambioPassword = false;
+				mi.tieneColaborador=false;
+				mi.edicionPermisos=false;
+				mi.cargandoPermisos=false; 
+				mi.usuariosSelected={usuario:"", email:"",password:"", usuarioCreo:"", fechaCreacion:"", usuarioActualizo:"", fechaActualizacion:"", colaborador:""};
+			}
+			
+		}
+
+		
 		mi.cargarTotalElementos(1);
 		//mi.cargarTabla(1);
-		
+
+		mi.buscarPermiso = function(tipo) {
+			
+			var modalInstance = $uibModal.open({
+			    animation : 'true',
+			    ariaLabelledBy : 'modal-title',
+			    ariaDescribedBy : 'modal-body',
+			    templateUrl : 'buscarPermiso.jsp',
+			    controller : 'modalBuscarPermiso',
+			    controllerAs : 'modalBuscar',
+			    backdrop : 'static',
+			    size : 'md',
+			    resolve : {
+			    	infoPermisos: function(){
+			    		var parametros={nuevo:mi.esNuevo, usuario:mi.usuariosSelected.usuario, tipo:tipo};
+			    		return  parametros;
+			    	}
+			    }
+
+			});
+
+			modalInstance.result.then(function(resultadoSeleccion) {
+				if(resultadoSeleccion.tipo===1){
+						mi.cargandoPermisos=true;
+						$http.post('/SRol',{accion:'getPermisosPorRol',id:resultadoSeleccion.rol.id}).success(
+								function(response){
+									mi.permisosAsignados=response.permisos;
+									mi.nuevosPermisos=response.ids;
+									mi.cargandoPermisos=false;
+								}
+						);
+				
+				}else{
+					mi.permisosAsignados.push(resultadoSeleccion);
+					mi.nuevosPermisos.push(resultadoSeleccion.id);
+				}
+				
+			}, function() {
+			});
+		}
 		
 		
 	}
 ]);
+
+
+
+/** Para buscar permiso **/
+app.controller('modalBuscarPermiso', [
+	'$uibModalInstance', '$scope', '$http', '$interval', 'i18nService',
+	'Utilidades', '$timeout', '$log','infoPermisos', modalBuscarPermiso
+]);
+
+function modalBuscarPermiso($uibModalInstance, $scope, $http, $interval, i18nService, $utilidades, $timeout, $log, infoPermisos) {
+
+	var mi = this;
+	if(infoPermisos.nuevo){
+		mi.mostrarCargando = true;
+		if(infoPermisos.tipo===1){
+			$http.post('/SRol', {
+	    		accion:'getRoles'
+	    	}).then(function(response) {
+	    	    if (response.data.success) {
+	    	    	mi.data = response.data.roles;
+	    	    	mi.opcionesGrid.data = mi.data;
+	    			mi.mostrarCargando = false;
+	    	    }
+	    	});
+			
+		}else{
+			$http.post('/SPermiso', {
+	    		accion:'getPermisos'
+	    	}).then(function(response) {
+	    	    if (response.data.success) {
+	    	    	mi.data = response.data.permisos;
+	    	    	mi.opcionesGrid.data = mi.data;
+	    			mi.mostrarCargando = false;
+	    	    }
+	    	});
+		}
+    	
+	}else{
+		 $http.post('/SUsuario', {
+		    	accion : 'getPermisosDisponibles',
+		    	usuario:infoPermisos.usuario
+		        }).success(function(response) {
+		        	mi.data = response.permisos;
+	    	    	mi.opcionesGrid.data = mi.data;
+	    			mi.mostrarCargando = false;
+		    });
+	}
+	mi.totalElementos = 0;
+	mi.paginaActual = 1;
+	mi.numeroMaximoPaginas = 5;
+	mi.elementosPorPagina = 9;
+
+	mi.mostrarCargando = false;
+	mi.data = [];
+
+	mi.itemSeleccionado = null;
+	mi.seleccionado = false;
+
+
+
+    mi.opcionesGrid = {
+		data : mi.data,
+		enableFiltering: true,
+		columnDefs : [
+			{
+				displayName : 'ID',
+				name : 'id',
+				cellClass : 'grid-align-right',
+				enableFiltering: true,
+				type : 'text', width : 150
+
+			},
+			{
+				displayName : 'nombre',
+				name : 'nombre',
+				cellClass : 'grid-align-right',
+				enableFiltering: true
+
+			},
+			{
+				displayName : 'Descripción',
+				name : 'descripcion',
+				cellClass : 'grid-align-left'
+			}
+		],
+		enableRowSelection : true,
+		enableRowHeaderSelection : false,
+		multiSelect : false,
+		modifierKeysToMultiSelect : false,
+		noUnselect : false,
+		enableFiltering : true,
+		enablePaginationControls : false,
+		paginationPageSize : 5,
+		onRegisterApi : function(gridApi) {
+		    mi.gridApi = gridApi;
+		    mi.gridApi.selection.on.rowSelectionChanged($scope,
+			    mi.seleccionarPermiso);
+		}
+    }
+
+    mi.seleccionarPermiso = function(row) {
+    	mi.itemSeleccionado = row.entity;
+    	mi.seleccionado = row.isSelected;
+    };
+
+
+     mi.ok = function() {
+    	if (mi.seleccionado) {
+    		if(infoPermisos.tipo===1){
+    			 $uibModalInstance.close({tipo:1, rol:mi.itemSeleccionado});
+    		}else{
+    			 $uibModalInstance.close(mi.itemSeleccionado);
+    		}
+    	   
+    	} else {
+    	    $utilidades.mensaje('warning', 'Debe seleccionar un permiso');
+    	}
+     };
+
+     mi.cancel = function() {
+    	$uibModalInstance.dismiss('cancel');
+     };
+}
+
 
--- sigpro/WebContent/app/components/usuarios/gestionUsuario.jsp
+++ sigpro/WebContent/app/components/usuarios/gestionUsuario.jsp
@@ -20,7 +20,7 @@
 			<div class="panel-heading"><h3>Usuarios</h3></div>
 		</div>
 
-		<div class="row" align="center" ng-hide="usuarioc.isCollapsed">
+		<div class="row" align="center" ng-hide="usuarioc.editandoUsuario">
 		<br>
 		
 			<div class="col-sm-12 operation_buttons" align="right">
@@ -91,9 +91,33 @@
 						</div>
 						</div>
 					</div>
-				     -->
-				    
-					<br> 
+				     -->				    
+					<br>
+					<div id="grid2" ui-grid="usuarioc.opcionesGrid" ui-grid-save-state ui-grid-move-columns ui-grid-resize-columns ui-grid-selection ui-grid-pinning ui-grid-pagination>
+				      <div class="grid_loading" ng-hide="!usuarioc.mostrarCargando">
+				        <div class="msg">
+				          <span><i class="fa fa-spinner fa-spin fa-4x"></i> <br /> <br /> <b>Cargando, por favor espere...</b> </span>
+				        </div>
+				      </div>
+				    </div>
+      				<br>
+			      	<div class="total-rows">
+					  Total de {{  usuarioc.totalElementos_ + (usuarioc.totalElementos_ == 1 ? " Colaborador" : " Colaboradores" ) }}
+				  	</div>
+      				<ul uib-pagination 
+      					total-items="usuarioc.totalElementos_" 
+				      	ng-model="usuarioc.paginaActual_" 
+				      	max-size="usuarioc.numeroMaximoPaginas_" 
+				      	items-per-page="usuarioc.elementosPorPagina_" 
+				      	first-text="Primero"
+				        last-text="Último" 
+				        next-text="Siguiente" 
+				        previous-text="Anterior" 
+				        class="pagination-sm" 
+				        boundary-links="true" 
+				        force-ellipses="true" 
+				        ng-change="usuarioc.cambiarPagina(2)"
+      				></ul> 
 				    </uib-tab>
 				  </uib-tabset>
     			
@@ -103,6 +127,125 @@
 		</div>
 		
 		<!-- inicio de edición -->
+		<div class="row second-main-form" ng-show="usuarioc.editandoUsuario">
+			<div class="page-header">
+				<h2 ng-hide="!usuarioc.esNuevo"><small>Nuevo Usuario</small></h2>
+				<h2 ng-hide="usuarioc.esNuevo"><small>Edición de Usuario</small></h2>
+			</div>
+				
+			<div class="col-sm-12 operation_buttons" align="right">
+				<div class="btn-group">
+					<shiro:hasPermission name="34020">
+						<label class="btn btn-success" ng-click="usuarioc.esNuevo ? (form1.$valid && !usuarioc.cargandoPermisos ? usuarioc.guardarUsuario() : '' ) :  (form.$valid  ? usuarioc.guardarUsuario() : '' )" 
+						ng-disabled="usuarioc.esNuevo ? form1.$invalid || usuarioc.cargandoPermisos : form.$invalid  || usuarioc.cargandoPermisos" uib-tooltip="Guardar">
+						<span class="glyphicon glyphicon-floppy-saved"></span>Guardar</label>
+					</shiro:hasPermission>
+			        <label class="btn btn-primary" ng-click="usuarioc.cancelar()"  uib-tooltip="Ir a Tabla">
+					<span class="glyphicon glyphicon-list-alt"></span>Ir a Tabla</label>
+	    		</div>
+	    	</div>
+			<div class="col-sm-12">
+				<form name="form">
+					<div class="form-group" ng-show="!usuarioc.esNuevo && !usuarioc.mostrarCambioPassword">
+						<input type="text" class="inputText" id="usuario" ng-model="usuarioc.usuariosSelected.usuario" 
+	    				ng-value="usuarioc.usuariosSelected.usuario" onblur="this.setAttribute('value', this.value);" readonly>
+	    				<label class="floating-label">* Usuario</label>
+					</div>
+					<div class="form-group" ng-show="!usuarioc.mostrarCambioPassword">
+	    				<input type="text" class="inputText" id="correo" ng-model="usuarioc.usuariosSelected.email" ng-required="true"
+	    				ng-value="usuarioc.usuariosSelected.email" onblur="this.setAttribute('value', this.value);">
+	    				<label class="floating-label">* Correo electrónico</label>
+					</div>
+					<div class="form-group" ng-show="!usuarioc.esNuevo">
+	    				<input type="password" class="inputText" id="password1" ng-model="usuarioc.usuariosSelected.password" ng-required="true"
+	    					ng-value="usuarioc.usuariosSelected.password" onblur="this.setAttribute('value', this.value);">
+	    				<label class="floating-label">* Contraseña</label>
+					</div>
+	
+				</form>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">	
+					<div align="center" ng-show="usuarioc.editandoUsuario">
+						<h3 ng-show="usuarioc.isCollapsed">Permisos</h3>
+						<div class="col-sm-12 operation_buttons" align="right" style="margin-left: -1%;" ng-if="usuarioc.esNuevo">
+							<div class="btn-group">
+								<label class="btn btn-default" ng-click="!usuarioc.cargandoPermisos? usuarioc.buscarPermiso(1) : ''"
+								ng-disabled="" uib-tooltip="Seleccionar Rol" ng-disabled="usuarioc.cargandoPermisos" >
+									<span class="glyphicon glyphicon-zoom-in"></span>
+									Seleccionar Rol
+								</label>
+								<label class="btn btn-default" ng-click="usuarioc.buscarPermiso(0)"
+								ng-disabled="" uib-tooltip="Agregar permiso" ng-disabled="usuarioc.cargandoPermisos">
+									<span class="glyphicon glyphicon-plus"></span>
+									Agregar permiso.
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-12 operation_buttons" align="right" style="margin-left: -1%;" ng-if="!usuarioc.esNuevo">
+							<div class="btn-group">
+								<label class="btn btn-default" ng-click="usuarioc.buscarPermiso(0)"
+								ng-disabled="" uib-tooltip="Agregar permiso" ng-disabled="usuarioc.cargandoPermisos">
+									<span class="glyphicon glyphicon-plus"></span>
+									Agregar permiso.
+								</label>
+							</div>
+						</div>			
+						<br>
+						<table style="width: 95%; overflow-y: scroll;height: 175px;display: block;" st-table="usuarioc.permisosAsignados" class="table table-striped  table-bordered table-hover table-propiedades">
+							<thead >
+								<tr>
+									<th style="width: 5%;">Nombre</th>
+									<th>Descripción</th>
+									<th style="width: 30px;">Quitar</th>
+		
+								</tr>
+							</thead>
+							<tbody>
+								<tr st-select-row="row"
+								ng-repeat="row in usuarioc.permisosAsignados">
+									<td>{{row.nombre}}</td>
+									<td>{{row.descripcion}}</td>
+									<td>
+										<button type="button"
+											ng-click="usuarioc.eliminarPermiso(row)"
+											class="btn btn-sm btn-danger">
+											<i class="glyphicon glyphicon-minus-sign"> </i>
+										</button>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div class="grid_loading" ng-if="usuarioc.cargandoPermisos" style="margin-top:80px; width: 96%; margin-left: 1%;">
+							<div class="msg">
+								<span><i class="fa fa-spinner fa-spin fa-4x"></i>
+									<br><br>
+									<b>Cargando, por favor espere...</b>
+								</span>
+							</div>
+						</div> 
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="col-sm-12 operation_buttons" align="right">
+				<div class="btn-group">
+			        <shiro:hasPermission name="34020">
+						<label class="btn btn-success" 
+						ng-click="usuarioc.esNuevo ? (form1.$valid && !usuarioc.cargandoPermisos ? usuarioc.guardarUsuario() : '' ) :  (form.$valid && !usuarioc.cargandoPermisos  ? usuarioc.guardarUsuario() : '' )" 
+						ng-disabled="usuarioc.esNuevo ? form1.$invalid || usuarioc.cargandoPermisos : form.$invalid  || usuarioc.cargandoPermisos" 
+						uib-tooltip="Guardar">
+							<span class="glyphicon glyphicon-floppy-saved"></span>
+							Guardar
+						</label>
+					</shiro:hasPermission>
+					<label class="btn btn-primary" ng-click="usuarioc.cancelar()"  uib-tooltip="Ir a Tabla">
+						<span class="glyphicon glyphicon-list-alt"></span>Ir a Tabla
+					</label>
+		    	</div>
+		    </div>	
+		</div>
+		<!-- fin de edicion de usuarios -->
 
 		
 
