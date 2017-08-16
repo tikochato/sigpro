f56168cc55ee8441666712ecfeb0ea252da37e30
Tikochato <daor01@gmail.com>
2017-08-16 17:43:36 -0600

    Inicio exportando Excel - Metas

--- sigpro/src/servlets/SPrestamoMetas.java
+++ sigpro/src/servlets/SPrestamoMetas.java
@@ -170,50 +170,6 @@ public class SPrestamoMetas extends HttpServlet {
 						response_text = String.join("", "{\"success\":false}");
 					}
 				}
-		
-		}else if(accion.equals("guardarProyectoMeta")){
-//			
-//			stmeta metavalor = new stmeta();
-//			metavalor.id = map.get("id")!=null ? Integer.parseInt(map.get("id")) : 0; 
-//			metavalor.fecha = map.get("fecha")!=null ? map.get("fecha") : "";
-//			metavalor.valor = map.get("valor")!=null ? map.get("valor") : "";
-//			metavalor.tipoMetaId = map.get("tipoMetaId")!=null ? Integer.parseInt(map.get("tipoMetaId")) : 0; 
-//			metavalor.unidadMedidaId = map.get("unidadMedidaId")!=null ? Integer.parseInt(map.get("unidadMedidaId")) : 0; 
-//			metavalor.objetoId = map.get("objetoId")!=null ? Integer.parseInt(map.get("objetoId")) : 0;
-//			metavalor.objetoTipoId = map.get("objetoTipoId")!=null ? Integer.parseInt(map.get("objetoTipoId")) : 0;
-//						
-//			metavalor = guardarProductoMetaValor(metavalor, usuario);
-//			
-//			response_text=new GsonBuilder().serializeNulls().create().toJson(metavalor);
-//	        response_text = String.join("", "\"metavalor\":",response_text);
-//	        response_text = String.join("", "{\"success\":true,", response_text,"}");
-//	        
-//	        response.setHeader("Content-Encoding", "gzip");
-//			response.setCharacterEncoding("UTF-8");
-//
-//	        OutputStream output = response.getOutputStream();
-//			GZIPOutputStream gz = new GZIPOutputStream(output);
-//	        gz.write(response_text.getBytes("UTF-8"));
-//	        gz.close();
-//	        output.close();
-		}else if(accion.equals("congelarPrestamoMeta")){
-//			Integer proyectoId = map.get("proyectoid")!=null ? Integer.parseInt(map.get("proyectoid")) : 0;
-//			Boolean exito = false;
-//			
-//			exito = congelarPrestamoMeta(proyectoId, usuario);
-//			
-//			response_text=new GsonBuilder().serializeNulls().create().toJson(exito);
-//	        response_text = String.join("", "\"metavalor\":",response_text);
-//	        response_text = String.join("", "{\"success\":true,", response_text,"}");
-//	        
-//	        response.setHeader("Content-Encoding", "gzip");
-//			response.setCharacterEncoding("UTF-8");
-//
-//	        OutputStream output = response.getOutputStream();
-//			GZIPOutputStream gz = new GZIPOutputStream(output);
-//	        gz.write(response_text.getBytes("UTF-8"));
-//	        gz.close();
-//	        output.close();
 		}else if (accion.equals("exportarExcel")){
 //			int proyectoId = Utils.String2Int(map.get("proyectoid"), 0);
 //			int anioInicio = Utils.String2Int(map.get("fechaInicio"), 0);
@@ -324,6 +280,183 @@ public class SPrestamoMetas extends HttpServlet {
 			anio.diciembre = new BigDecimal[2];
 			
 		return anio;		
+	}
+	
+	private byte[] exportarExcel(int prestamoId, int anioInicio, int anioFin, int agrupacion, String filas, String columnas, String usuario) throws IOException{
+		byte [] outArray = null;
+//		CExcel excel = new CExcel("PrestamoMetas",false);
+//		JsonParser parser = new JsonParser();
+//		
+//		//Cabeceras
+//		JsonElement tablaJson = parser.parse(columnas);
+//		JsonArray columnaArreglo = tablaJson.getAsJsonArray();
+//		Object[] categoriasObject = new Object[columnaArreglo.size()];
+//		Object[] columnasObject = new Object[columnaArreglo.size()];
+//		
+//		int i=0;
+//		String categoriaAnterior="";
+//		for (JsonElement columnaElement : columnaArreglo){
+//			JsonObject columna = columnaElement.getAsJsonObject();
+//			columnasObject[i] = columna.get("displayName").getAsString();
+//			String categoria = (columna.get("category") == null || columna.get("category").isJsonNull()) ? "" : columna.get("category").getAsString();
+//			if (!categoriaAnterior.equals(categoria)){
+//				categoriasObject[i] = categoria;
+//				categoriaAnterior = categoria;
+//			}
+//			if(categoria.trim().isEmpty()){
+//				categoriasObject[i]=columnasObject[i];
+//				columnasObject[i]="";
+//			}
+//			i++;
+//		}						
+//		
+//		Map<String,Object[]> datos = new HashMap<>();
+//		datos.put("0", categoriasObject);
+//		datos.put("1", columnasObject);
+//		int filaId = 2;
+//		
+//		//Filas
+//		tablaJson = parser.parse(filas);
+//		JsonArray tablaArreglo = tablaJson.getAsJsonArray();
+//		
+//		
+//		for (JsonElement filaElement : tablaArreglo){
+//			Object[] filasObject = new Object[columnaArreglo.size()];
+//			JsonObject fila = filaElement.getAsJsonObject();
+//			
+//			String sangria;
+//			switch (fila.get("objetoTipo").getAsInt()){
+//				case 1: sangria = ""; break;
+//				case 2: sangria = "\t"; break;
+//				case 3: sangria = "\t\t"; break;
+//				case 4: sangria = "\t\t\t"; break;
+//				case 5: sangria = "\t\t\t\t"; break;
+//				default: sangria = "";
+//			}
+//			int unidadDeMedidaId = fila.get("unidadDeMedidaId").isJsonNull() ? 0 : Integer.parseInt(fila.get("unidadDeMedidaId").getAsString()); 
+//			String unidadDeMedidaNombre = "";
+//			if (unidadDeMedidaId >0){
+//				unidadDeMedidaNombre = MetaUnidadMedidaDAO.getMetaUnidadMedidaPorId(unidadDeMedidaId).getNombre();
+//			}
+//			
+//			filasObject[0] = sangria + fila.get("nombreMeta").getAsString();
+//			filasObject[1] = fila.get("fechaInicio").getAsString();
+//			filasObject[2] = fila.get("fechaFin").getAsString();
+//			filasObject[3] = unidadDeMedidaNombre;
+//			i=4;
+//			Double totalPlanificado = new Double(0);
+//			Double totalReal = new Double(0);
+//			int objetoTipo = fila.get("objetoTipo").getAsInt();
+//			if (objetoTipo == OBJETO_ID_PRODUCTO){
+//				if (agrupacion == AGRUPACION_MES){ 
+//					for (int anioInicioC = anioInicio; anioInicioC<=(anioFin); anioInicioC++){
+//						for (int mes = 0; mes < 12; mes++){
+//							if(fila.get(mes+"P"+anioInicioC)!=null && !fila.get(mes+"P"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"P"+anioInicioC).getAsString();
+//								totalPlanificado+=fila.get(mes+"P"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//							if(fila.get(mes+"R"+anioInicioC)!=null && !fila.get(mes+"R"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"R"+anioInicioC).getAsString();
+//								totalReal+=fila.get(mes+"R"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//						}
+//					}
+//				}else if (agrupacion == AGRUPACION_CUATRIMESTRE){ 
+//					for (int anioInicioC = anioInicio; anioInicioC<=(anioFin); anioInicioC++){
+//						for (int contador = 1; contador < 4; contador++){
+//							int mes = (contador*4)-1;
+//							if(fila.get(mes+"P"+anioInicioC)!=null && !fila.get(mes+"P"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"P"+anioInicioC).getAsString();
+//								totalPlanificado+=fila.get(mes+"P"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//							if(fila.get(mes+"R"+anioInicioC)!=null && !fila.get(mes+"R"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"R"+anioInicioC).getAsString();
+//								totalReal+=fila.get(mes+"R"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//						}
+//					}
+//				}else if (agrupacion == AGRUPACION_SEMESTRE){ 
+//					for (int anioInicioC = anioInicio; anioInicioC<=(anioFin); anioInicioC++){
+//						for (int contador = 1; contador < 3; contador++){
+//							int mes = (contador*6)-1;
+//							if(fila.get(mes+"P"+anioInicioC)!=null && !fila.get(mes+"P"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"P"+anioInicioC).getAsString();
+//								totalPlanificado+=fila.get(mes+"P"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//							if(fila.get(mes+"R"+anioInicioC)!=null && !fila.get(mes+"R"+anioInicioC).isJsonNull()){
+//								filasObject[i] = fila.get(mes+"R"+anioInicioC).getAsString();
+//								totalReal+=fila.get(mes+"R"+anioInicioC).getAsDouble();
+//							}
+//							i++;
+//						}
+//					}
+//				}else if (agrupacion == AGRUPACION_ANUAL){ 
+//					for (int anioInicioC = anioInicio; anioInicioC<=(anioFin); anioInicioC++){
+//						if(fila.get("11P"+anioInicioC)!=null && !fila.get("11P"+anioInicioC).isJsonNull()){
+//							filasObject[i] = fila.get("11P"+anioInicioC).getAsString();
+//							totalPlanificado+=fila.get("11P"+anioInicioC).getAsDouble();
+//						}
+//						i++;
+//						if(fila.get("11R"+anioInicioC)!=null && !fila.get("11R"+anioInicioC).isJsonNull()){
+//							filasObject[i] = fila.get("11R"+anioInicioC).getAsString();
+//							totalReal+=fila.get("11R"+anioInicioC).getAsDouble();
+//						}
+//						i++;
+//					}
+//				}
+//				filasObject[columnaArreglo.size()-3] = totalPlanificado;
+//				filasObject[columnaArreglo.size()-2] = totalReal;
+//				filasObject[columnaArreglo.size()-1] = fila.get("metaFinal").getAsString();
+//			}	
+//			
+//			datos.put(filaId+"", filasObject);
+//			filaId++;
+//		}
+//			
+//		
+//		String path = excel.ExportarExcel(datos, "Metas de Préstamo", usuario);
+//
+//		
+//		File file=new File(path);
+//		if(file.exists()){
+//	        FileInputStream is = null;
+//	        try {
+//	        	is = new FileInputStream(file);
+//	        }
+//	        catch (Exception e) {
+//	        	
+//	        }
+//	        //
+//	        ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+//	        
+//	        int readByte = 0;
+//	        byte[] buffer = new byte[2024];
+//
+//            while(true)
+//            {
+//                readByte = is.read(buffer);
+//                if(readByte == -1)
+//                {
+//                    break;
+//                }
+//                outByteStream.write(buffer);
+//            }
+//            
+//            file.delete();
+//            
+//            is.close();
+//            outByteStream.flush();
+//            outByteStream.close();
+//            
+//	        outArray = Base64.encode(outByteStream.toByteArray());
+//		
+//		}
+		return outArray;
 	}
 	
 //
