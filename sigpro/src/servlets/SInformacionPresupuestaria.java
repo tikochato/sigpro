ca4579e897c573ff114e36a1df03b718c6ed73c1
Tikochato <daor01@gmail.com>
2017-09-07 11:50:57 -0600

    Logs Excel

--- sigpro/src/servlets/SInformacionPresupuestaria.java
+++ sigpro/src/servlets/SInformacionPresupuestaria.java
@@ -126,26 +126,26 @@ public class SInformacionPresupuestaria extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 		try{
-			Exception exception = new Exception("SinformacionPresupuestaria.java - ignore");
 			
-		    CLogger.writeFullConsole("Inicia doPost - ", exception);
+		    CLogger.write_simple("1", SInformacionPresupuestaria.class, "Inicia doPost - 130");
 			
 			request.setCharacterEncoding("UTF-8");
 			HttpSession sesionweb = request.getSession();
 			String usuario = sesionweb.getAttribute("usuario")!= null ? sesionweb.getAttribute("usuario").toString() : null;
 			
-		    CLogger.writeFullConsole("Gson - ", exception);
+
+		    CLogger.write_simple("2", SInformacionPresupuestaria.class, "Inicia doPost - 137");
 			
 			Gson gson = new Gson();
 			Type type = new TypeToken<Map<String, String>>(){}.getType();
 			
-		    CLogger.writeFullConsole("StringBuilder - ", exception);
+			CLogger.write_simple("3", SInformacionPresupuestaria.class, "StringBuilder - 142");
 		    
 			StringBuilder sb = new StringBuilder();
 			BufferedReader br = request.getReader();
 			String str;
 			
-		    CLogger.writeFullConsole("while str - ", exception);
+			CLogger.write_simple("4", SInformacionPresupuestaria.class, "while str - 148");
 		    
 			while ((str = br.readLine()) != null) {
 				sb.append(str);
@@ -154,9 +154,13 @@ public class SInformacionPresupuestaria extends HttpServlet {
 			String accion = map.get("accion")!=null ? map.get("accion") : "";
 			String response_text = "";
 			
-		    CLogger.writeFullConsole("if accion  - ", exception);
+			CLogger.write_simple("5", SInformacionPresupuestaria.class, "if accion - ");
 		    
 			if(accion.equals("generarInforme")){
+				
+
+			    CLogger.write_simple("1", SInformacionPresupuestaria.class, "Generar Informe - 162");
+			    
 				Integer idPrestamo = Utils.String2Int(map.get("idPrestamo"),0);
 				Integer anioInicial = Utils.String2Int(map.get("anioInicial"),0);
 				Integer anioFinal = Utils.String2Int(map.get("anioFinal"),0);
@@ -170,8 +174,7 @@ public class SInformacionPresupuestaria extends HttpServlet {
 					response_text = String.join("", "{\"success\":false}");
 				}
 			}else if(accion.equals("exportarExcel")){
-				
-			    CLogger.writeFullConsole("accion: exportarExcel  - ", exception);
+				CLogger.write_simple("6", SInformacionPresupuestaria.class, "accion: exportarExcel - 173");
 				
 				Integer idPrestamo = Utils.String2Int(map.get("idPrestamo"),0);
 				Integer anioInicial = Utils.String2Int(map.get("anioInicial"),0);
@@ -179,11 +182,11 @@ public class SInformacionPresupuestaria extends HttpServlet {
 				Integer agrupacion = Utils.String2Int(map.get("agrupacion"), 0);
 				Integer tipoVisualizacion = Utils.String2Int(map.get("tipoVisualizacion"), 0);
 				
-			    CLogger.writeFullConsole("exportarExcel() - ", exception);
+				CLogger.write_simple("7", SInformacionPresupuestaria.class, "exportarExcel() - 181");
 				
 		        byte [] outArray = exportarExcel(idPrestamo, anioInicial, anioFinal, agrupacion, tipoVisualizacion, usuario);
 			
-			    CLogger.writeFullConsole("repuesta exportarExcel()- ", exception);
+		        CLogger.write_simple("8", SInformacionPresupuestaria.class, "repuesta exportarExcel() - 185");
 		        
 				response.setContentType("application/ms-excel");
 				response.setContentLength(outArray.length);
