d8c9c7ae79a579d9fe1fdb707fa25b59d1f132e4
rafa <rafa@172.19.2.78>
2017-09-13 11:23:07 -0600

    bug

--- sigpro/WebContent/app/components/prestamo/proyecto.controller.js
+++ sigpro/WebContent/app/components/prestamo/proyecto.controller.js
@@ -1178,11 +1178,11 @@ function buscarPorProyecto($uibModalInstance, $rootScope,$scope, $http, $interva
 		mi.ejercicio = $entidad.ejercicio;
 		for(var i=current_year-$rootScope.catalogo_entidades_anos; i<=current_year; i++)
 			mi.ejercicios.push(i);
-		mi.ejercicio = (mi.ejercicio === undefined) ? current_year : mi.ejercicio;
+		mi.ejercicio = (mi.ejercicio == "") ? current_year : mi.ejercicio;
 		$http.post('SEntidad', { accion: 'entidadesporejercicio', ejercicio: mi.ejercicio}).success(function(response) {
 			mi.entidades = response.entidades;
 			if(mi.entidades.length>0){
-				mi.entidad = (mi.entidad=="") ? mi.entidades[0] : mi.entidad;
+				mi.entidad = (mi.entidad===undefined) ? mi.entidades[0] : mi.entidad;
 				$accionServlet.ejercicio = mi.ejercicio;
 				$accionServlet.entidad = mi.entidad.entidad;
 				$http.post($servlet, $accionServlet).success(function(response) {
