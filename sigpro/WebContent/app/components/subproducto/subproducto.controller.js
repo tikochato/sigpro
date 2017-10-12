99d9a6ec0b9f437616037daabb323bafad605ded
rafa <rafa@172.19.2.78>
2017-10-12 10:55:06 -0600

    Corrección de bug de subproudcto tipo

--- sigpro/WebContent/app/components/subproducto/subproducto.controller.js
+++ sigpro/WebContent/app/components/subproducto/subproducto.controller.js
@@ -397,7 +397,7 @@ function controlSubproducto($rootScope,$scope, $routeParams, $route, $window, $l
 			mi.esForma = true;
 			mi.entityselected = null;
 			mi.esNuevo = false;
-			mi.tipo = mi.subproducto.subProductoTipoId;
+			mi.tipo = mi.subproducto.subProductoTipoId; 
 			mi.tipoNombre = mi.subproducto.subProductoTipo;
 			
 			if(mi.subproducto.duracionDimension == 'd'){
