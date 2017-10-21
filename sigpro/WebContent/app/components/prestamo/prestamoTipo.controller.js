999d6b5d3cb96758ade5cd1da4808f16c655b382
ppenan <pedro_penan@hotmail.com>
2017-10-20 19:19:48 -0600

    tipo de prestamo

---
+++ sigpro/WebContent/app/components/prestamo/prestamoTipo.controller.js
@@ -0,0 +1,235 @@
+var app = angular.module('prestamotipoController', [ 'ngTouch']);
+
+app.controller('prestamotipoController',['$scope','$rootScope','$http','$interval','i18nService','Utilidades','$routeParams','$window','$location','$route','uiGridConstants','$mdDialog','$q','$uibModal', 'dialogoConfirmacion', 
+	function($scope, $rootScope, $http, $interval,i18nService,$utilidades,$routeParams,$window,$location,$route,uiGridConstants,$mdDialog,$q,$uibModal, $dialogoConfirmacion) {
+		var mi=this;
+
+		$window.document.title = $utilidades.sistema_nombre+' - Tipo '+$rootScope.etiquetas.prestamo;
+		i18nService.setCurrentLang('es');
+		mi.mostrarcargando=true;
+		mi.prestamotipos = [];
+		mi.prestamotipo;
+		mi.mostraringreso=false;
+		mi.esnuevo = false;
+		mi.totalPestamotipos = 0;
+		mi.paginaActual = 1;
+		mi.numeroMaximoPaginas = $utilidades.numeroMaximoPaginas;
+		mi.elementosPorPagina = $utilidades.elementosPorPagina;
+		
+		mi.columnaOrdenada=null;
+		mi.ordenDireccion = null;
+		
+		mi.filtros = [];
+		mi.orden = null;
+
+		mi.mostrarcargandoProyProp=true;
+		
+		mi.editarElemento = function (event) {
+	        var filaId = angular.element(event.toElement).scope().rowRenderIndex;
+	        mi.gridApi.selection.selectRow(mi.gridOptions.data[filaId]);
+	        mi.editar();
+	    };
+
+		mi.gridOptions = {
+				enableRowSelection : true,
+				enableRowHeaderSelection : false,
+				multiSelect: false,
+				modifierKeysToMultiSelect: false,
+				noUnselect: true,
+				enableFiltering: true,
+				enablePaginationControls: false,
+			    paginationPageSize: $utilidades.elementosPorPagina,
+			    useExternalFiltering: true,
+			    useExternalSorting: true,
+			    rowTemplate: '<div ng-dblclick="grid.appScope.prestamotipoc.editarElemento($event)" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" ui-grid-one-bind-id-grid="rowRenderIndex + \'-\' + col.uid + \'-cell\'" class="ui-grid-cell ng-scope ui-grid-disable-selection grid-align-right" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }" role="gridcell" ui-grid-cell="" ></div>',
+				columnDefs : [
+					{ name: 'id', width: 100, displayName: 'ID', cellClass: 'grid-align-right', type: 'number', enableFiltering: false },
+				    { name: 'nombre', width: 200, displayName: 'Nombre',cellClass: 'grid-align-left'
+				    	,filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.prestamotipoc.filtros[\'nombre\']" ng-keypress="grid.appScope.prestamotipoc.filtrar($event)" style="width:175px;"></input></div>'
+				    },
+				    { name: 'descripcion', displayName: 'Descripción', cellClass: 'grid-align-left', enableFiltering: false},
+				    { name: 'usuarioCreo', displayName: 'Usuario Creación' 
+				    	,filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.prestamotipoc.filtros[\'usuario_creo\']" ng-keypress="grid.appScope.prestamotipoc.filtrar($event)"></input></div>'
+				    },
+				    { name: 'fechaCreacion', displayName: 'Fecha Creación', cellClass: 'grid-align-right', type: 'date', cellFilter: 'date:\'dd/MM/yyyy\'',
+				    	filterHeaderTemplate: '<div class="ui-grid-filter-container"><input type="text" style="width: 90%;" ng-model="grid.appScope.prestamotipoc.filtros[\'fecha_creacion\']" ng-keypress="grid.appScope.prestamotipoc.filtrar($event)" ></input></div>'
+				    }
+				],
+				onRegisterApi: function(gridApi) {
+					mi.gridApi = gridApi;
+					gridApi.selection.on.rowSelectionChanged($scope,function(row) {
+						mi.prestamotipo = row.entity;
+					});
+					
+					
+					gridApi.core.on.sortChanged( $scope, function ( grid, sortColumns ) {
+							if(sortColumns.length==1){
+								grid.appScope.prestamotipoc.columnaOrdenada=sortColumns[0].field;
+								grid.appScope.prestamotipoc.ordenDireccion = sortColumns[0].sort.direction;
+								grid.appScope.prestamotipoc.cargarTabla(grid.appScope.prestamotipoc.paginaActual);
+							}
+							else if(sortColumns.length>1){
+								sortColumns[0].unsort();
+							}
+							else{
+								if(grid.appScope.prestamotipoc.columnaOrdenada!=null){
+									grid.appScope.prestamotipoc.columnaOrdenada=null;
+									grid.appScope.prestamotipoc.ordenDireccion=null;
+								}
+							}
+								
+						} );
+					
+
+					if($routeParams.reiniciar_vista=='rv'){
+						mi.guardarEstado();
+						mi.obtenerTotalprestamotipos();
+				    }
+				    else{
+				    	  $http.post('/SEstadoTabla', { action: 'getEstado', grid:'prestamoTipos', t: (new Date()).getTime()}).then(function(response){
+				    		  if(response.data.success && response.data.estado!='')
+				    			  mi.gridApi.saveState.restore( $scope, response.data.estado);
+					    	  mi.gridApi.colMovable.on.columnPositionChanged($scope, mi.guardarEstado);
+						      mi.gridApi.colResizable.on.columnSizeChanged($scope, mi.guardarEstado);
+						      mi.gridApi.core.on.columnVisibilityChanged($scope, mi.guardarEstado);
+						      mi.obtenerTotalprestamotipos();
+						  });
+				    }
+				}
+		};
+		mi.redireccionSinPermisos=function(){
+			$window.location.href = '/main.jsp#!/forbidden';		
+		}
+		mi.cargarTabla = function(pagina){
+			mi.mostrarcargando=true;
+			$http.post('/SPrestamoTipo', { accion: 'getPrestamoTipoPagina', pagina: pagina, numeroprestamotipo: $utilidades.elementosPorPagina
+				,filtro_nombre: mi.filtros['nombre'] 
+				,filtro_usuario_creo: mi.filtros['usuario_creo']
+			    ,filtro_fecha_creacion: mi.filtros['fecha_creacion']
+			    ,columna_ordenada: mi.columnaOrdenada, orden_direccion: mi.ordenDireccion, t: (new Date()).getTime()
+			    }).success(
+			
+					function(response) {
+						mi.prestamotipos = response.poryectotipos;
+						mi.gridOptions.data = mi.prestamotipos;
+						mi.mostrarcargando = false;
+					});
+		}
+
+		mi.guardar=function(){
+			if(mi.prestamotipo!=null  && mi.prestamotipo.nombre!=''){
+				$http.post('/SPrestamoTipo', {
+					accion: 'guardarprestamotipo',
+					esnuevo: mi.esnuevo,
+					id: mi.prestamotipo.id,
+					nombre: mi.prestamotipo.nombre,
+					descripcion: mi.prestamotipo.descripcion,
+				}).success(function(response){
+					if(response.success){
+						$utilidades.mensaje('success','Tipo de '+$rootScope.etiquetas.prestamo+' '+(mi.esnuevo ? 'creado' : 'guardado')+' con éxito');
+						mi.esnuevo = false;
+						mi.prestamotipo.id = response.id;
+						mi.obtenerTotalprestamotipos();
+
+					}
+					else
+						$utilidades.mensaje('danger','Error al '+(mi.esnuevo ? 'crear' : 'guardar')+' el Tipo de '+$rootScope.etiquetas.prestamo);
+				});
+			}
+			else
+				$utilidades.mensaje('warning','Debe de llenar todos los campos obligatorios');
+		};
+
+		mi.editar = function() {
+			if(mi.prestamotipo!=null && mi.prestamotipo.id!=null){
+				mi.mostraringreso = true;
+				mi.esnuevo = false;
+				mi.cargarTotalPropiedades();
+				$utilidades.setFocus(document.getElementById("nombre"));
+			}
+			else
+				$utilidades.mensaje('warning','Debe seleccionar el Tipo de '+$rootScope.etiquetas.prestamo+' que desea editar');
+		}
+
+
+		mi.borrar = function(ev) {
+			if(mi.prestamotipo!=null && mi.prestamotipo.id!=null){
+				$dialogoConfirmacion.abrirDialogoConfirmacion($scope
+						, "Confirmación de Borrado"
+						, '¿Desea borrar el tipo de '+$rootScope.etiquetas.prestamo+' "'+mi.prestamotipo.nombre+'"?'
+						, "Borrar"
+						, "Cancelar")
+				.result.then(function(data) {
+					if(data){
+						$http.post('/SPrestamoTipo', {
+							accion: 'borrarprestamoTipo',
+							id: mi.prestamotipo.id
+						}).success(function(response){
+							if(response.success){
+								$utilidades.mensaje('success','Tipo de '+$rootScope.etiquetas.prestamo+' borrado con éxito');
+								mi.prestamotipo = null;
+								mi.obtenerTotalprestamotipos();
+							}
+							else
+								$utilidades.mensaje('danger','Error al borrar el Tipo '+$rootScope.etiquetas.prestamo);
+						});
+					}
+				}, function(){
+					
+				});
+			}
+			else
+				$utilidades.mensaje('warning','Debe seleccionar el Tipo de '+$rootScope.etiquetas.prestamo+' que desea borrar');
+		};
+
+		mi.nuevo = function() {
+			mi.mostraringreso=true;
+			mi.esnuevo = true;
+			mi.prestamotipo = {};
+			mi.gridApi.selection.clearSelectedRows();
+			mi.cargarTotalPropiedades();
+			$utilidades.setFocus(document.getElementById("nombre"));
+		};
+
+		mi.irATabla = function() {
+			mi.mostraringreso=false;
+		}
+
+		mi.guardarEstado=function(){
+			var estado = mi.gridApi.saveState.save();
+			var tabla_data = { action: 'guardaEstado', grid:'prestamotipos', estado: JSON.stringify(estado), t: (new Date()).getTime() };
+			$http.post('/SEstadoTabla', tabla_data).then(function(response){
+
+			});
+		}
+
+		mi.cambioPagina=function(){
+			mi.cargarTabla(mi.paginaActual);
+		}
+
+		mi.reiniciarVista=function(){
+			if($location.path()=='/prestamotipo/rv')
+				$route.reload();
+			else
+				$location.path('/prestamotipo/rv');
+		}
+
+		mi.filtrar = function(evt){
+			if(evt.keyCode==13){
+				mi.obtenerTotalprestamotipos();
+				mi.gridApi.selection.clearSelectedRows();
+				mi.prestamotipo = null;
+			}
+		}
+		
+		mi.obtenerTotalprestamotipos = function(){
+			$http.post('/SPrestamoTipo', { accion: 'numeroprestamoTipos',
+				filtro_nombre: mi.filtros['nombre'], 
+				filtro_usuario_creo: mi.filtros['usuarioCreo'], filtro_fecha_creacion: mi.filtros['fechaCreacion'], t: (new Date()).getTime() }).then(
+					function(response) {
+						mi.totalprestamotipos = response.data.totalprestamotipos;
+						mi.paginaActual = 1;
+						mi.cargarTabla(mi.paginaActual);
+			});
+		}
+} ]);
---
+++ sigpro/src/servlets/SPrestamoTipo.java
@@ -0,0 +1,188 @@
+package servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.zip.GZIPOutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.joda.time.DateTime;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import dao.ProyectoTipoDAO;
+import dao.PtipoPropiedadDAO;
+import pojo.ProyectoPropiedad;
+import pojo.ProyectoTipo;
+import pojo.PtipoPropiedad;
+import pojo.PtipoPropiedadId;
+import utilities.Utils;
+
+@WebServlet("/SPrestamoTipo")
+public class SPrestamoTipo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	class stprestamotipo{
+		Integer id;
+		String nombre;
+		String descripcion;
+		String usuarioCreo;
+		String usuarioActualizo;
+		String fechaCreacion;
+		String fechaActualizacion;
+		int estado;
+	}
+	
+    public SPrestamoTipo() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		HttpSession sesionweb = request.getSession();
+		String usuario = sesionweb.getAttribute("usuario")!= null ? sesionweb.getAttribute("usuario").toString() : null;
+		Gson gson = new Gson();
+		Type type = new TypeToken<Map<String, String>>(){}.getType();
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = request.getReader();
+		String str;
+		while ((str = br.readLine()) != null) {
+			sb.append(str);
+		}
+		;
+		Map<String, String> map = gson.fromJson(sb.toString(), type);
+		String accion = map.get("accion");
+		String response_text="";
+		if(accion.equals("getPrestamoTipoPagina")){
+			int pagina = map.get("pagina")!=null  ? Integer.parseInt(map.get("pagina")) : 0;
+			int numeroPrestamosTipos = map.get("numeroprestamotipo")!=null  ? Integer.parseInt(map.get("numeroprestamotipo")) : 0;
+			String filtro_nombre = map.get("filtro_nombre");
+			String filtro_usuario_creo = map.get("filtro_usuario_creo");
+			String filtro_fecha_creacion = map.get("filtro_fecha_creacion");
+			String columna_ordenada = map.get("columna_ordenada");
+			String orden_direccion = map.get("orden_direccion");
+			
+			
+			List<PrestamoTipo> proyectotipos = ProyectoTipoDAO.getProyectosTipoPagina(pagina, numeroPrestamosTipos, filtro_nombre, filtro_usuario_creo, filtro_fecha_creacion, columna_ordenada, orden_direccion);
+			List<stprestamotipo> stcooperantes=new ArrayList<stprestamotipo>();
+			for(ProyectoTipo proyectotipo:proyectotipos){
+				stprestamotipo temp =new stprestamotipo();
+				temp.id = proyectotipo.getId();
+				temp.nombre = proyectotipo.getNombre();
+				temp.descripcion = proyectotipo.getDescripcion();
+				
+				temp.estado = proyectotipo.getEstado();
+				temp.fechaActualizacion = Utils.formatDateHour(proyectotipo.getFechaActualizacion());
+				temp.fechaCreacion = Utils.formatDateHour(proyectotipo.getFechaCreacion());
+				temp.usuarioActualizo = proyectotipo.getUsuarioActualizo();
+				temp.usuarioCreo = proyectotipo.getUsarioCreo();
+				stcooperantes.add(temp);
+			}
+			
+			response_text=new GsonBuilder().serializeNulls().create().toJson(stcooperantes);
+	        response_text = String.join("", "\"poryectotipos\":",response_text);
+	        response_text = String.join("", "{\"success\":true,", response_text,"}");
+		}
+		else if(accion.equals("numeroProyectoTipos")){
+			String filtro_nombre = map.get("filtro_nombre");
+			String filtro_usuario_creo = map.get("filtro_usuario_creo");
+			String filtro_fecha_creacion = map.get("filtro_fecha_creacion");
+			response_text = String.join("","{ \"success\": true, \"totalproyectotipos\":",ProyectoTipoDAO.getTotalProyectoTipos(filtro_nombre, filtro_usuario_creo, filtro_fecha_creacion).toString()," }");
+		}
+		else if(accion.equals("guardarProyectotipo")){
+			boolean result = false;
+			boolean esnuevo = map.get("esnuevo").equals("true");
+			int id = map.get("id")!=null ? Integer.parseInt(map.get("id")) : 0;
+			if(id>0 || esnuevo){
+				
+				String nombre = map.get("nombre");
+				String descripcion = map.get("descripcion");
+				ProyectoTipo proyectoTipo;
+				
+				if(esnuevo){
+					proyectoTipo = new ProyectoTipo(nombre, descripcion, 
+							usuario,null,new DateTime().toDate(),null,1,null,new HashSet<PtipoPropiedad>(0));
+				}
+				else{
+					proyectoTipo = ProyectoTipoDAO.getProyectoTipoPorId(id);
+					proyectoTipo.setNombre(nombre);
+					proyectoTipo.setDescripcion(descripcion);
+					proyectoTipo.setUsuarioActualizo(usuario);
+					proyectoTipo.setFechaActualizacion(new DateTime().toDate());
+					Set<PtipoPropiedad> propiedades_temp = proyectoTipo.getPtipoPropiedads();
+					proyectoTipo.setPtipoPropiedads(new HashSet<PtipoPropiedad>(0));
+					if (propiedades_temp!=null){
+						for (PtipoPropiedad ptipoPropiedad : propiedades_temp){
+							PtipoPropiedadDAO.eliminarTotalPtipoPropiedad(ptipoPropiedad);
+						}
+					}
+				}
+				
+				result = ProyectoTipoDAO.guardarProyectoTipo(proyectoTipo);
+				
+				String[] idsPropiedades =  map.get("propiedades") != null && map.get("propiedades").length()>0 ? map.get("propiedades").toString().split(",") : null;
+				if (idsPropiedades !=null && idsPropiedades.length>0){
+					for (String idPropiedad : idsPropiedades){
+						PtipoPropiedadId ptipoPropiedadId = new PtipoPropiedadId(proyectoTipo.getId(), Integer.parseInt(idPropiedad));
+						ProyectoPropiedad proyectoPropiedad = new ProyectoPropiedad();
+						proyectoPropiedad.setId(Integer.parseInt(idPropiedad));
+						
+						PtipoPropiedad ptipoPropiedad = new PtipoPropiedad(
+								ptipoPropiedadId,
+								proyectoPropiedad,
+								proyectoTipo, 
+								usuario, new DateTime().toDate());
+						
+						ptipoPropiedad.setProyectoTipo(proyectoTipo);
+						if (proyectoTipo.getPtipoPropiedads() == null){
+							proyectoTipo.setPtipoPropiedads(new HashSet<PtipoPropiedad>(0));
+						}
+						proyectoTipo.getPtipoPropiedads().add(ptipoPropiedad);
+					}
+				}
+				
+				result = ProyectoTipoDAO.guardarProyectoTipo(proyectoTipo);
+				response_text = String.join("","{ \"success\": ",(result ? "true" : "false"),", "
+						+ "\"id\": " + proyectoTipo.getId() +" }");
+			}
+			else
+				response_text = "{ \"success\": false }";
+		}
+		else if(accion.equals("borrarProyectoTipo")){
+			int id = map.get("id")!=null ? Integer.parseInt(map.get("id")) : 0;
+			if(id>0){
+				ProyectoTipo proyectoTipo = ProyectoTipoDAO.getProyectoTipoPorId(id);
+				response_text = String.join("","{ \"success\": ",(ProyectoTipoDAO.eliminarProyectoTipo(proyectoTipo) ? "true" : "false")," }");
+			}
+			else
+				response_text = "{ \"success\": false }";
+		}
+		response.setHeader("Content-Encoding", "gzip");
+		response.setCharacterEncoding("UTF-8");
+		
+        OutputStream output = response.getOutputStream();
+		GZIPOutputStream gz = new GZIPOutputStream(output);
+        gz.write(response_text.getBytes("UTF-8"));
+        gz.close();
+        output.close();
+
+	}
+}
