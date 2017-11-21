26742337c25672c57a3228a45151b9afe61a6ca1
david <ing.david.o@gmail.com>
2017-11-21 10:51:24 -0600

    pojos con trigger

--- sigpro/src/pojo/AsignacionRaci.java
+++ sigpro/src/pojo/AsignacionRaci.java
@@ -1,5 +1,5 @@
 package pojo;
-// Generated Nov 20, 2017 4:56:36 PM by Hibernate Tools 5.2.3.Final
+// Generated Nov 21, 2017 10:47:44 AM by Hibernate Tools 5.2.3.Final
 
 import java.util.Date;
 import javax.persistence.Column;
